07724974909e1442d4b330fefb54e9ddc495c3e5
Karina Thomas <karina.thomas@bbc.co.uk>
2024-10-29 13:20:56 +0000

    Fix tests for zh-tw

--- src/app/legacy/psammead/psammead-locales/moment/zh-tw.test.js
+++ src/app/legacy/psammead/psammead-locales/moment/zh-tw.test.js
@@ -42,24 +42,24 @@ describe('zh-tw', () => {
 
   const a = [
     ['dddd, MMMM Do YYYY, a h:mm:ss', '星期日, 二月 14日 2010, 下午 3:25:50'],
-    ['ddd, Ah', '周日, 下午3'],
+    ['ddd, Ah', '週日, 下午3'],
     ['M Mo MM MMMM MMM', '2 2月 02 二月 2月'],
     ['YYYY YY', '2010 10'],
     ['D Do DD', '14 14日 14'],
-    ['d do dddd ddd dd', '0 0日 星期日 周日 日'],
+    ['d do dddd ddd dd', '0 0日 星期日 週日 日'],
     ['DDD DDDo DDDD', '45 45日 045'],
-    ['w wo ww', '6 6周 06'],
+    ['w wo ww', '8 8週 08'],
     ['h hh', '3 03'],
     ['H HH', '15 15'],
     ['m mm', '25 25'],
     ['s ss', '50 50'],
     ['a A', '下午 下午'],
-    ['[这年的第] DDDo', '这年的第 45日'],
+    ['[這年的第] DDDo', '這年的第 45日'],
     ['LTS', '15:25:50'],
     ['L', '2010/02/14'],
     ['LL', '2010年2月14日'],
     ['LLL', '2010年2月14日下午3時25分'],
-    ['LLLL', '2010年2月14日星期日下午3点25分'],
+    ['LLLL', '2010年2月14日星期日 15:25'],
     ['l', '2010/2/14'],
     ['ll', '2010年2月14日'],
     ['lll', '2010年2月14日 15:25'],
@@ -129,7 +129,7 @@ describe('zh-tw', () => {
 
   describe('format week', () => {
     const expected =
-      '星期日 周日 日_星期一 周一 一_星期二 周二 二_星期三 周三 三_星期四 周四 四_星期五 周五 五_星期六 周六 六'.split(
+      '星期日 週日 日_星期一 週一 一_星期二 週二 二_星期三 週三 三_星期四 週四 四_星期五 週五 五_星期六 週六 六'.split(
         '_'
       );
     let i;
@@ -144,11 +144,6 @@ describe('zh-tw', () => {
 
   describe('from', () => {
     const start = moment([2007, 1, 28]);
-    assert.equal(
-      start.from(moment([2007, 1, 28]).add({ s: 44 }), true),
-      '几秒',
-      '44 seconds = a few seconds'
-    );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ s: 45 }), true),
       '1 分鐘',
@@ -226,42 +221,42 @@ describe('zh-tw', () => {
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ d: 26 }), true),
-      '1 个月',
+      '1 個月',
       '26 days = a month'
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ d: 30 }), true),
-      '1 个月',
+      '1 個月',
       '30 days = a month'
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ d: 43 }), true),
-      '1 个月',
+      '1 個月',
       '43 days = a month'
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ d: 46 }), true),
-      '2 个月',
+      '2 個月',
       '46 days = 2 months'
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ d: 74 }), true),
-      '2 个月',
+      '2 個月',
       '75 days = 2 months'
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ d: 76 }), true),
-      '3 个月',
+      '3 個月',
       '76 days = 3 months'
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ M: 1 }), true),
-      '1 个月',
+      '1 個月',
       '1 month = a month'
     );
     assert.equal(
       start.from(moment([2007, 1, 28]).add({ M: 5 }), true),
-      '5 个月',
+      '5 個月',
       '5 months = 5 months'
     );
     assert.equal(
@@ -287,14 +282,14 @@ describe('zh-tw', () => {
   });
 
   describe('suffix', () => {
-    assert.equal(moment(30000).from(0), '几秒后', 'prefix');
-    assert.equal(moment(0).from(30000), '几秒前', 'prefix');
+    assert.equal(moment(30000).from(0), '幾秒後', 'prefix');
+    assert.equal(moment(0).from(30000), '幾秒前', 'prefix');
   });
 
   describe('now from now', () => {
     assert.equal(
       moment().fromNow(),
-      '几秒前',
+      '幾秒前',
       'now from now should display as in the past'
     );
   });
@@ -302,10 +297,10 @@ describe('zh-tw', () => {
   describe('fromNow', () => {
     assert.equal(
       moment().add({ s: 30 }).fromNow(),
-      '几秒后',
+      '幾秒後',
       'in a few seconds'
     );
-    assert.equal(moment().add({ d: 5 }).fromNow(), '5 天后', 'in 5 days');
+    assert.equal(moment().add({ d: 5 }).fromNow(), '5 天後', 'in 5 days');
   });
 
   describe('calendar day', () => {
@@ -313,32 +308,32 @@ describe('zh-tw', () => {
 
     assert.equal(
       moment(calendarTime).calendar(),
-      '今天12:00',
+      '今天 12:00',
       'today at the same time'
     );
     assert.equal(
       moment(calendarTime).add({ m: 25 }).calendar(),
-      '今天12:25',
+      '今天 12:25',
       'Now plus 25 min'
     );
     assert.equal(
       moment(calendarTime).add({ h: 1 }).calendar(),
-      '今天13:00',
+      '今天 13:00',
       'Now plus 1 hour'
     );
     assert.equal(
       moment(calendarTime).add({ d: 1 }).calendar(),
-      '明天12:00',
+      '明天 12:00',
       'tomorrow at the same time'
     );
     assert.equal(
       moment(calendarTime).subtract({ h: 1 }).calendar(),
-      '今天11:00',
+      '今天 11:00',
       'Now minus 1 hour'
     );
     assert.equal(
       moment(calendarTime).subtract({ d: 1 }).calendar(),
-      '昨天12:00',
+      '昨天 12:00',
       'yesterday at the same time'
     );
   });
@@ -348,45 +343,24 @@ describe('zh-tw', () => {
     let m;
 
     for (i = 2; i < 7; i += 1) {
-      const week = moment().week();
       m = moment().add({ d: i });
-      if (week === m.week()) {
-        assert.equal(
-          m.calendar(),
-          m.format('[本]dddLT'),
-          `Today + ${i} days current time`
-        );
-        m.hours(0).minutes(0).seconds(0).milliseconds(0);
-        assert.equal(
-          m.calendar(),
-          m.format('[本]dddLT'),
-          `Today + ${i} days beginning of day`
-        );
-        m.hours(23).minutes(59).seconds(59).milliseconds(999);
-        assert.equal(
-          m.calendar(),
-          m.format('[本]dddLT'),
-          `Today + ${i} days end of day`
-        );
-      } else {
-        assert.equal(
-          m.calendar(),
-          m.format('[下]dddLT'),
-          `Today + ${i} days current time`
-        );
-        m.hours(0).minutes(0).seconds(0).milliseconds(0);
-        assert.equal(
-          m.calendar(),
-          m.format('[下]dddLT'),
-          `Today + ${i} days beginning of day`
-        );
-        m.hours(23).minutes(59).seconds(59).milliseconds(999);
-        assert.equal(
-          m.calendar(),
-          m.format('[下]dddLT'),
-          `Today + ${i} days end of day`
-        );
-      }
+      assert.equal(
+        m.calendar(),
+        m.format('[下]dddd LT'),
+        `Today + ${i} days current time`
+      );
+      m.hours(0).minutes(0).seconds(0).milliseconds(0);
+      assert.equal(
+        m.calendar(),
+        m.format('[下]dddd LT'),
+        `Today + ${i} days beginning of day`
+      );
+      m.hours(23).minutes(59).seconds(59).milliseconds(999);
+      assert.equal(
+        m.calendar(),
+        m.format('[下]dddd LT'),
+        `Today + ${i} days end of day`
+      );
     }
   });
 
@@ -395,45 +369,24 @@ describe('zh-tw', () => {
     let m;
 
     for (i = 2; i < 7; i += 1) {
-      const week = moment().week();
       m = moment().subtract({ d: i });
-      if (week !== m.week()) {
-        assert.equal(
-          m.calendar(),
-          m.format('[上]dddLT'),
-          `Today - ${i} days current time`
-        );
-        m.hours(0).minutes(0).seconds(0).milliseconds(0);
-        assert.equal(
-          m.calendar(),
-          m.format('[上]dddLT'),
-          `Today - ${i} days beginning of day`
-        );
-        m.hours(23).minutes(59).seconds(59).milliseconds(999);
-        assert.equal(
-          m.calendar(),
-          m.format('[上]dddLT'),
-          `Today - ${i} days end of day`
-        );
-      } else {
-        assert.equal(
-          m.calendar(),
-          m.format('[本]dddLT'),
-          `Today - ${i} days current time`
-        );
-        m.hours(0).minutes(0).seconds(0).milliseconds(0);
-        assert.equal(
-          m.calendar(),
-          m.format('[本]dddLT'),
-          `Today - ${i} days beginning of day`
-        );
-        m.hours(23).minutes(59).seconds(59).milliseconds(999);
-        assert.equal(
-          m.calendar(),
-          m.format('[本]dddLT'),
-          `Today - ${i} days end of day`
-        );
-      }
+      assert.equal(
+        m.calendar(),
+        m.format('[上]dddd LT'),
+        `Today - ${i} days current time`
+      );
+      m.hours(0).minutes(0).seconds(0).milliseconds(0);
+      assert.equal(
+        m.calendar(),
+        m.format('[上]dddd LT'),
+        `Today - ${i} days beginning of day`
+      );
+      m.hours(23).minutes(59).seconds(59).milliseconds(999);
+      assert.equal(
+        m.calendar(),
+        m.format('[上]dddd LT'),
+        `Today - ${i} days end of day`
+      );
     }
   });
 
@@ -462,18 +415,28 @@ describe('zh-tw', () => {
   describe('weeks year starting sunday formatted', () => {
     assert.equal(
       moment([2012, 0, 1]).format('w ww wo'),
-      '52 52 52周',
-      'Jan  1 2012 应该是第52周'
+      '1 01 1週',
+      'Jan  1 2012 應該是第 1週'
     );
     assert.equal(
       moment([2012, 0, 7]).format('w ww wo'),
-      '1 01 1周',
-      'Jan  7 2012 应该是第 1周'
+      '1 01 1週',
+      'Jan  7 2012 應該是第 1週'
+    );
+    assert.equal(
+      moment([2012, 0, 8]).format('w ww wo'),
+      '2 02 2週',
+      'Jan  8 2012 應該是第 2週'
     );
     assert.equal(
       moment([2012, 0, 14]).format('w ww wo'),
-      '2 02 2周',
-      'Jan 14 2012 应该是第 2周'
+      '2 02 2週',
+      'Jan 14 2012 應該是第 2週'
+    );
+    assert.equal(
+      moment([2012, 0, 15]).format('w ww wo'),
+      '3 03 3週',
+      'Jan 15 2012 應該是第 3週'
     );
   });
 });
