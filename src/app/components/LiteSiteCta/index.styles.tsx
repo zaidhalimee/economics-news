d0200e46d96585273b43f7ea7f0b4037413a7b03
HarveyPeachey <harvey.peachey@bbc.co.uk>
2024-10-29 16:57:25 +0000

    adds spacing changes and hcm fixes

--- src/app/components/LiteSiteCta/index.styles.tsx
+++ src/app/components/LiteSiteCta/index.styles.tsx
@@ -2,33 +2,39 @@ import { css, Theme } from '@emotion/react';
 import pixelsToRem from '../../utilities/pixelsToRem';
 
 export default {
-  outerContainer: ({ palette }: Theme) =>
+  outerContainer: ({ palette, mq, spacings }: Theme) =>
     css({
+      margin: `0 ${spacings.FULL}rem`,
       backgroundColor: `${palette.WHITE}`,
       borderBottom: `${pixelsToRem(1)}rem solid ${palette.GREY_3}`,
+      [mq.GROUP_2_MIN_WIDTH]: {
+        margin: `0 ${spacings.DOUBLE}rem`,
+      },
     }),
   container: ({ spacings, mq }: Theme) =>
     css({
-      margin: `0 ${spacings.FULL}rem`,
-      padding: `${spacings.TRIPLE}rem 0 ${spacings.FULL}rem 0`,
+      padding: `${spacings.DOUBLE}rem 0 0 0`,
       maxWidth: '63.4rem',
       position: 'relative',
-      [mq.GROUP_2_MIN_WIDTH]: {
-        margin: `0 ${spacings.DOUBLE}rem`,
+      [mq.GROUP_1_MIN_WIDTH]: {
+        padding: `${spacings.TRIPLE}rem 0 ${spacings.FULL}rem 0`,
       },
       [mq.GROUP_4_MIN_WIDTH]: {
         margin: `0 auto`,
       },
     }),
-  message: ({ spacings }: Theme) =>
+  message: ({ spacings, mq }: Theme) =>
     css({
       marginBottom: `${spacings.HALF}rem`,
+      [mq.GROUP_1_MIN_WIDTH]: {
+        marginBottom: `${spacings.FULL}rem`,
+      },
     }),
-  chevron: ({ palette, spacings }: Theme) =>
+  chevron: ({ palette, spacings, mq }: Theme) =>
     css({
       color: palette.GREY_10,
       fill: 'currentColor',
-      marginInlineStart: `${spacings.HALF}rem`,
+      marginInlineStart: `${spacings.FULL}rem`,
       verticalAlign: 'middle',
       width: `${pixelsToRem(14)}rem`,
       height: `${pixelsToRem(14)}rem`,
@@ -38,19 +44,31 @@ export default {
       'a:focus &, a:hover &': {
         color: palette.POSTBOX,
       },
+      [mq.FORCED_COLOURS]: {
+        fill: 'linkText',
+        'a:visited &': {
+          fill: 'visitedText',
+        },
+        'a:active &': {
+          fill: 'activeText',
+        },
+      },
     }),
   link: () =>
     css({
       display: 'inline-block',
       textDecoration: 'none',
     }),
-  bottomLinkSpacing: ({ spacings }: Theme) =>
+  bottomLinkSpacing: ({ spacings, mq }: Theme) =>
     css({
-      padding: `${spacings.FULL}rem 0 ${spacings.DOUBLE}rem`,
+      padding: `${spacings.HALF}rem 0 ${spacings.DOUBLE}rem`,
+      [mq.GROUP_1_MIN_WIDTH]: {
+        padding: `${spacings.FULL}rem 0 ${spacings.DOUBLE}rem`,
+      },
     }),
   topLinkSpacing: ({ spacings }: Theme) =>
     css({
-      padding: `${spacings.DOUBLE}rem 0 ${spacings.FULL}rem`,
+      padding: `${spacings.FULL + spacings.HALF}rem 0 ${spacings.FULL + spacings.HALF}rem`,
     }),
   linkText: ({ palette }: Theme) =>
     css({
