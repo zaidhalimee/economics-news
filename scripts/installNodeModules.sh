21134ea213e6bba5667642b0fb411d1267a1f2e4
alex-magana <alex.magana@andela.com>
2024-10-30 19:43:03 +0300

    Add flag

--- scripts/installNodeModules.sh
+++ scripts/installNodeModules.sh
@@ -3,10 +3,10 @@ if [ "$1" = "--production" ]; then
   echo "Removing node modules"
   rm -rf node_modules
   echo "Installing 'production' node modules"
-  yarn workspaces focus --production
+  yarn workspaces focus --production --verbose
 else
   echo "Removing node modules"
   rm -rf node_modules
   echo "Installing all node modules"
-  yarn install --immutable --check-cache
+  yarn install --verbose --immutable --check-cache
 fi
