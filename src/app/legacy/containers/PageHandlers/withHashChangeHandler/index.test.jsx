5bf08f0d185fab5be524cc1bb3cc94eaaa03a383
Aaron Moore <aaron.moore@bbc.co.uk>
2024-11-05 12:25:57 +0000

    Update index.test.jsx

--- src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
+++ src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
@@ -42,7 +42,9 @@ describe('withHashChangeHandler', () => {
     render(
       <CyrillicFixture
         location={{
-          hash: '#%D0%9C%D0%B8%D1%80%D0%BD%D0%B8%D1%98%D0%B0-%D1%81%D0%B0%D0%BC-%D0%BD%D0%B5%D0%BA%D0%BE-%D0%B1%D1%80%D0%B8%D0%BD%D0%B5-%D0%BE-%D1%9A%D0%BE%D1%98-%D0%B4%D0%BE%D0%BA-%D1%98%D0%B5-%D1%83-%D1%88%D0%BA%D0%BE%D0%BB%D0%B8',
+          hash:
+            // This is the encoded version of Мирнија-сам-неко-брине-о-њој-док-је-у-школи
+            '#%D0%9C%D0%B8%D1%80%D0%BD%D0%B8%D1%98%D0%B0-%D1%81%D0%B0%D0%BC-%D0%BD%D0%B5%D0%BA%D0%BE-%D0%B1%D1%80%D0%B8%D0%BD%D0%B5-%D0%BE-%D1%9A%D0%BE%D1%98-%D0%B4%D0%BE%D0%BA-%D1%98%D0%B5-%D1%83-%D1%88%D0%BA%D0%BE%D0%BB%D0%B8',
         }}
       />,
     );
