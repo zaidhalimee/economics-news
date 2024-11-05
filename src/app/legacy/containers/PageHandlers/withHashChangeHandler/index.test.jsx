f0326eea1dafc40ad02716f3f3eb0963423400e5
Aaron Moore <aaron.moore@bbc.co.uk>
2024-11-05 12:24:15 +0000

    Update index.test.jsx

--- src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
+++ src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
@@ -38,18 +38,12 @@ describe('withHashChangeHandler', () => {
     expect(window.HTMLElement.prototype.focus).toHaveBeenCalledTimes(1);
   });
 
-  it('should scroll into view and focus on element when hash location changes with Cyrillic characters', async () => {
-    const { rerender } = render(
+  it('should scroll into view and focus on element when hash location contains Cyrillic characters', async () => {
+    render(
       <CyrillicFixture
         location={{
           hash: '#%D0%9C%D0%B8%D1%80%D0%BD%D0%B8%D1%98%D0%B0-%D1%81%D0%B0%D0%BC-%D0%BD%D0%B5%D0%BA%D0%BE-%D0%B1%D1%80%D0%B8%D0%BD%D0%B5-%D0%BE-%D1%9A%D0%BE%D1%98-%D0%B4%D0%BE%D0%BA-%D1%98%D0%B5-%D1%83-%D1%88%D0%BA%D0%BE%D0%BB%D0%B8',
         }}
-      />,
-    );
-
-    rerender(
-      <CyrillicFixture
-        location={{ hash: '#Мирнија-сам-неко-брине-о-њој-док-је-у-школи' }}
       />,
     );
 
