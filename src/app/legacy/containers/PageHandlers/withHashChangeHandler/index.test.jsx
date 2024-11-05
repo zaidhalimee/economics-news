7a42a779172281ab31f3f7d93762f405fc122e52
Aaron Moore <aaron.moore@bbc.co.uk>
2024-11-05 11:53:29 +0000

    Update index.test.jsx

--- src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
+++ src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
@@ -22,30 +22,40 @@ const CyrillicFixture = withHashChangeHandler(() => (
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 window.HTMLElement.prototype.focus = jest.fn();
 
-it('should scroll into view and focus on element when hash location changes', async () => {
-  const { rerender } = render(<Fixture location={{ hash: '' }} />);
+describe('withHashChangeHandler', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  rerender(<Fixture location={{ hash: '#section-1' }} />);
+  it('should scroll into view and focus on element when hash location changes', async () => {
+    const { rerender } = render(<Fixture location={{ hash: '' }} />);
 
-  expect(window.HTMLElement.prototype.scrollIntoView).toHaveBeenCalledTimes(1);
-  expect(window.HTMLElement.prototype.focus).toHaveBeenCalledTimes(1);
-});
+    rerender(<Fixture location={{ hash: '#section-1' }} />);
 
-it('should scroll into view and focus on element when hash location changes with Cyrillic characters', async () => {
-  const { rerender } = render(
-    <CyrillicFixture
-      location={{
-        hash: '#%D0%9C%D0%B8%D1%80%D0%BD%D0%B8%D1%98%D0%B0-%D1%81%D0%B0%D0%BC-%D0%BD%D0%B5%D0%BA%D0%BE-%D0%B1%D1%80%D0%B8%D0%BD%D0%B5-%D0%BE-%D1%9A%D0%BE%D1%98-%D0%B4%D0%BE%D0%BA-%D1%98%D0%B5-%D1%83-%D1%88%D0%BA%D0%BE%D0%BB%D0%B8',
-      }}
-    />,
-  );
+    expect(window.HTMLElement.prototype.scrollIntoView).toHaveBeenCalledTimes(
+      1,
+    );
+    expect(window.HTMLElement.prototype.focus).toHaveBeenCalledTimes(1);
+  });
 
-  rerender(
-    <CyrillicFixture
-      location={{ hash: '#Мирнија-сам-неко-брине-о-њој-док-је-у-школи' }}
-    />,
-  );
+  it('should scroll into view and focus on element when hash location changes with Cyrillic characters', async () => {
+    const { rerender } = render(
+      <CyrillicFixture
+        location={{
+          hash: '#%D0%9C%D0%B8%D1%80%D0%BD%D0%B8%D1%98%D0%B0-%D1%81%D0%B0%D0%BC-%D0%BD%D0%B5%D0%BA%D0%BE-%D0%B1%D1%80%D0%B8%D0%BD%D0%B5-%D0%BE-%D1%9A%D0%BE%D1%98-%D0%B4%D0%BE%D0%BA-%D1%98%D0%B5-%D1%83-%D1%88%D0%BA%D0%BE%D0%BB%D0%B8',
+        }}
+      />,
+    );
 
-  expect(window.HTMLElement.prototype.scrollIntoView).toHaveBeenCalledTimes(1);
-  expect(window.HTMLElement.prototype.focus).toHaveBeenCalledTimes(1);
+    rerender(
+      <CyrillicFixture
+        location={{ hash: '#Мирнија-сам-неко-брине-о-њој-док-је-у-школи' }}
+      />,
+    );
+
+    expect(window.HTMLElement.prototype.scrollIntoView).toHaveBeenCalledTimes(
+      1,
+    );
+    expect(window.HTMLElement.prototype.focus).toHaveBeenCalledTimes(1);
+  });
 });
