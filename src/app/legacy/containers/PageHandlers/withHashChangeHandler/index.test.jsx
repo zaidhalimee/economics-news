0ca1933cc3b1988daa16e745f80967e8f06891e5
Aaron Moore <aaron.moore@bbc.co.uk>
2024-11-05 12:30:08 +0000

    Add Hindi fixture

--- src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
+++ src/app/legacy/containers/PageHandlers/withHashChangeHandler/index.test.jsx
@@ -19,6 +19,13 @@ const CyrillicFixture = withHashChangeHandler(() => (
   </>
 ));
 
+const HindiFixture = withHashChangeHandler(() => (
+  <>
+    <a href="#आंखों-के-सामने-छा-गया-था-अंधेरा">Go to section 1</a>
+    <section id="आंखों-के-सामने-छा-गया-था-अंधेरा">Section 1</section>
+  </>
+));
+
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 window.HTMLElement.prototype.focus = jest.fn();
 
@@ -45,6 +52,23 @@ describe('withHashChangeHandler', () => {
           hash:
             // This is the encoded version of Мирнија-сам-неко-брине-о-њој-док-је-у-школи
             '#%D0%9C%D0%B8%D1%80%D0%BD%D0%B8%D1%98%D0%B0-%D1%81%D0%B0%D0%BC-%D0%BD%D0%B5%D0%BA%D0%BE-%D0%B1%D1%80%D0%B8%D0%BD%D0%B5-%D0%BE-%D1%9A%D0%BE%D1%98-%D0%B4%D0%BE%D0%BA-%D1%98%D0%B5-%D1%83-%D1%88%D0%BA%D0%BE%D0%BB%D0%B8',
+        }}
+      />,
+    );
+
+    expect(window.HTMLElement.prototype.scrollIntoView).toHaveBeenCalledTimes(
+      1,
+    );
+    expect(window.HTMLElement.prototype.focus).toHaveBeenCalledTimes(1);
+  });
+
+  it('should scroll into view and focus on element when hash location contains Hindi characters', async () => {
+    render(
+      <HindiFixture
+        location={{
+          hash:
+            // This is the encoded version of आंखों-के-सामने-छा-गया-था-अंधेरा
+            '#%E0%A4%86%E0%A4%82%E0%A4%96%E0%A5%8B%E0%A4%82-%E0%A4%95%E0%A5%87-%E0%A4%B8%E0%A4%BE%E0%A4%AE%E0%A4%A8%E0%A5%87-%E0%A4%9B%E0%A4%BE-%E0%A4%97%E0%A4%AF%E0%A4%BE-%E0%A4%A5%E0%A4%BE-%E0%A4%85%E0%A4%82%E0%A4%A7%E0%A5%87%E0%A4%B0%E0%A4%BE',
         }}
       />,
     );
