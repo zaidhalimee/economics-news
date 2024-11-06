7c93c6c92cf8d5a07e1afd7d964e50bb55d49ac8
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-11-06 15:19:22 +0000

    add unit tests

---
+++ src/app/components/JumpTo/index.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '../react-testing-library-with-providers';
+import JumpTo, { JumpToProps } from './index';
+import pidginArticleFixtureWithJumpToBlock from './fixtureData';
+
+describe('JumpTo Component', () => {
+  const jumpToBlock =
+    pidginArticleFixtureWithJumpToBlock.data.article.content.model.blocks.find(
+      block => block.type === 'jumpTo',
+    );
+
+  const jumpToHeadings = jumpToBlock?.model.jumpToHeadings ?? [];
+
+  const defaultProps: JumpToProps = {
+    jumpToHeadings,
+    eventTrackingData: {
+      componentName: 'jumpto',
+    },
+  };
+
+  test('renders the jump-to title', () => {
+    render(<JumpTo {...defaultProps} />);
+    const title = screen.getByText('Jump to');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('renders the correct number of headings', () => {
+    render(<JumpTo {...defaultProps} />);
+    const headings = screen.getAllByRole('listitem');
+    expect(headings.length).toBe(jumpToHeadings.length);
+  });
+});
