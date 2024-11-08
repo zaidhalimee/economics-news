bcefd93c9abffbb9dca0ed5f7e69aaa44304e96b
Andrew Bennett <andrew.bennett07@bbc.co.uk>
2024-11-08 15:56:27 +0000

    console log moment plugin

--- src/app/legacy/psammead/moment-timezone-include/src/writeToNestedFile/index.js
+++ src/app/legacy/psammead/moment-timezone-include/src/writeToNestedFile/index.js
@@ -3,6 +3,8 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 
 const writeToNestedFile = (filePath, contents) => {
+  // eslint-disable-next-line no-console
+  console.log('MOMENT_PATH: ' + path.join(__dirname, filePath));
   const fullFilePath = path.join(__dirname, filePath);
   const folder = path.dirname(fullFilePath);
 
