446b5d413eda1aee8c9c817cee6d36d120fb8365
“LilyL0u” <lily.higham@bbc.co.uk>
2024-11-10 19:22:01 +0000

    related content and most read in list with content shift

--- src/app/components/JumpTo/index.tsx
+++ src/app/components/JumpTo/index.tsx
@@ -33,7 +33,21 @@ const JumpTo = ({ jumpToHeadings }: JumpToProps) => {
   useEffect(() => {
     setHash(window.location.hash);
   }, []);
-
+  // state initialisation. useState(false) initialises a state varirable
+  // and isRelatedContentPresent is set to the default value of false
+  // isRelatedContentPresent is the state variable that holds the current state
+  // indicating whether the related content is present or not
+  // setIsRelatedContentPresent is the function used to update the state variable isRelatedContentPresent
+  const [isRelatedContentPresent, setIsRelatedContentPresent] = useState(false);
+  // This hook runs after the component renders
+  useEffect(() => {
+    // this line tries to find the related content element with the id
+    const relatedContentHeading = document.getElementById(
+      'related-content-heading',
+    );
+    // this double negation turns the result into a boolean, where if it is found the boolean is true, and if it is null because it is not found, then it is false
+    setIsRelatedContentPresent(!!relatedContentHeading);
+  }, []); // the empty array as the second argument to useEffect means that this effect only runs once (after the initial render)
   const linkClickHandler = (
     e: React.MouseEvent<HTMLElement, MouseEvent>,
     subheadingId: string,
@@ -61,6 +75,7 @@ const JumpTo = ({ jumpToHeadings }: JumpToProps) => {
       >
         {jumpTo}
       </Text>
+
       <ol role="list" css={styles.list}>
         {jumpToHeadings?.map(({ heading }) => {
           const sanitisedId = idSanitiser(heading);
@@ -85,6 +100,47 @@ const JumpTo = ({ jumpToHeadings }: JumpToProps) => {
             </li>
           );
         })}
+
+        {isRelatedContentPresent && (
+          <li key="#related-content-heading" css={styles.listItem}>
+            <a
+              href="#related-content-heading"
+              onClick={e => linkClickHandler(e, '#related-content-heading')}
+              css={styles.link}
+              aria-labelledby="jump-to-related-content-heading"
+            >
+              <span
+                id="jump-to-related-content-heading"
+                css={[
+                  styles.linkText,
+                  decodeURIComponent(hash) === '#related-content-heading' &&
+                    styles.linkTextActive,
+                ]}
+              >
+                Related Content
+              </span>
+            </a>
+          </li>
+        )}
+        <li key="#Most-Read" css={styles.listItem}>
+          <a
+            href="#Most-Read"
+            onClick={e => linkClickHandler(e, '#Most-Read')}
+            css={styles.link}
+            aria-labelledby="jump-to-most-read-heading"
+          >
+            <span
+              id="jump-to-most-read-heading"
+              css={[
+                styles.linkText,
+                decodeURIComponent(hash) === '#Most-Read' &&
+                  styles.linkTextActive,
+              ]}
+            >
+              Most Read
+            </span>
+          </a>
+        </li>
       </ol>
     </nav>
   );
