6ef9764eb5475759d29a9bfa78cb68e984a97c74
louisearchibald <louise.archibald@bbc.co.uk>
2024-11-14 17:36:41 +0000

    import order

--- src/app/pages/ArticlePage/index.tsx
+++ src/app/pages/ArticlePage/index.tsx
@@ -1,6 +1,6 @@
+import withOptimizelyProvider from '#app/legacy/containers/PageHandlers/withOptimizelyProvider';
 import ArticlePage from './ArticlePage';
 import applyBasicPageHandlers from '../utils/applyBasicPageHandlers';
-import withOptimizelyProvider from '#app/legacy/containers/PageHandlers/withOptimizelyProvider';
 
 const OptimizelyArticle = withOptimizelyProvider(ArticlePage);
 
