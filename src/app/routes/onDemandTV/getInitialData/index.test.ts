ac172207e4227afdea861f3068669ab1dfdb3333
Karina Thomas <karina.thomas@bbc.co.uk>
2024-11-18 14:53:10 +0000

    Convert getInitialData to TS; Invoke the BFF instead of the JSON endpoint; Implement toggles logic to determine the number of recent episodes to display

--- src/app/routes/onDemandTV/getInitialData/index.test.ts
+++ src/app/routes/onDemandTV/getInitialData/index.test.ts
@@ -1,9 +1,4 @@
-import pipe from 'ramda/src/pipe';
-import dissocPath from 'ramda/src/dissocPath';
-import map from 'ramda/src/map';
-import loggerMock from '#testHelpers/loggerMock';
 import onDemandTvJson from '#data/pashto/bbc_pashto_tv/tv_programmes/w13xttn4';
-import { TV_MISSING_FIELD } from '#lib/logger.const';
 import { MEDIA_PAGE } from '#app/routes/utils/pageTypes';
 import * as fetchPageData from '../../utils/fetchPageData';
 import getInitialData from '.';
@@ -76,75 +71,5 @@ describe('Get initial data for on demand tv', () => {
 
     expect(pageData.recentEpisodes.length).toEqual(3);
     expect(pageData.recentEpisodes[0].id).toEqual('w172zmsln64wk50');
-  });
-
-  it('should override renderer on test', async () => {
-    process.env.SIMORGH_APP_ENV = 'test';
-    await getInitialData({ path: 'mock-live-tv-path', pageType: MEDIA_PAGE });
-    expect(spy).toHaveBeenCalledWith({
-      path: 'mock-live-tv-path?renderer_env=live',
-      pageType: MEDIA_PAGE,
-    });
-  });
-
-  it('should not override renderer on live', async () => {
-    process.env.SIMORGH_APP_ENV = 'live';
-    await getInitialData({ path: 'mock-live-tv-path', pageType: MEDIA_PAGE });
-    expect(spy).toHaveBeenCalledWith({
-      path: 'mock-live-tv-path',
-      pageType: MEDIA_PAGE,
-    });
-  });
-
-  it('invokes logging when expected data is missing in fetchData response', async () => {
-    const errorFields = [
-      ['metadata', 'id'],
-      ['metadata', 'createdBy'],
-      ['content', 'blocks', 0, 'id'],
-    ];
-
-    const warnFields = [
-      ['promo', 'headlines', 'headline'],
-      ['metadata', 'releaseDateTimeStamp'],
-    ];
-
-    const infoFields = [
-      ['metadata', 'language'],
-      ['metadata', 'title'],
-      ['promo', 'media', 'synopses', 'short'],
-      ['promo', 'media', 'synopses', 'medium'],
-      ['metadata', 'analyticsLabels', 'contentType'],
-      ['metadata', 'analyticsLabels', 'pageTitle'],
-      ['metadata', 'analyticsLabels', 'pageIdentifier'],
-      ['promo', 'media', 'versions', 0, 'durationISO8601'],
-      ['promo', 'media', 'imageUrl'],
-      ['promo', 'brand', 'title'],
-      ['content', 'blocks', 0, 'imageUrl'],
-      ['content', 'blocks', 0, 'episodeTitle'],
-    ];
-
-    const pageDataWithMissingFields = pipe(
-      ...map(dissocPath, [...errorFields, ...warnFields, ...infoFields]),
-      JSON.stringify,
-    )(onDemandTvJson);
-
-    fetch.mockResponse(pageDataWithMissingFields);
-
-    await getInitialData({
-      path: 'mock-on-demand-tv-path',
-      pageType: MEDIA_PAGE,
-      toggles: {
-        recentVideoEpisodes: { enabled: false, value: 4 },
-      },
-    });
-
-    const countMissingFieldCalls = mockedFunction =>
-      mockedFunction.mock.calls.filter(
-        ([logCategory]) => logCategory === TV_MISSING_FIELD,
-      ).length;
-
-    expect(countMissingFieldCalls(loggerMock.error)).toBe(errorFields.length);
-    expect(countMissingFieldCalls(loggerMock.warn)).toBe(warnFields.length);
-    expect(countMissingFieldCalls(loggerMock.info)).toBe(infoFields.length);
   });
 });
---
+++ src/app/routes/onDemandTV/getInitialData/index.ts
@@ -0,0 +1,59 @@
+import fetchDataFromBFF from '#app/routes/utils/fetchDataFromBFF';
+import { InitialDataProps } from '#app/models/types/initialData';
+import handleError from '#app/routes/utils/handleError';
+import { BFF_FETCH_ERROR } from '#app/lib/logger.const';
+import nodeLogger from '#app/lib/logger.node';
+import { FetchError } from '#app/models/types/fetch';
+import { TV_PAGE } from '#app/routes/utils/pageTypes';
+
+const logger = nodeLogger(__filename);
+
+export default async ({
+  service,
+  variant,
+  path: pathname,
+  getAgent,
+  toggles,
+}: InitialDataProps) => {
+  try {
+    const { status, json } = await fetchDataFromBFF({
+      pathname,
+      service,
+      variant,
+      pageType: TV_PAGE,
+      getAgent,
+    });
+
+    if (!json?.data) {
+      throw handleError('On Demand TV data is malformed', 500);
+    }
+
+    const data = json?.data;
+
+    const maxRecentEpisodes =
+      // @ts-expect-error recentVideoEpisodes does exist on the toggles object
+      toggles?.recentVideoEpisodes?.enabled
+        ? // @ts-expect-error recentVideoEpisodes does exist on the toggles object
+          toggles?.recentVideoEpisodes?.value || 4
+        : 0;
+
+    return {
+      status,
+      pageData: {
+        ...data,
+        recentEpisodes: data.recentEpisodes.slice(0, maxRecentEpisodes),
+      },
+    };
+  } catch (error: unknown) {
+    const { message, status } = error as FetchError;
+
+    logger.error(BFF_FETCH_ERROR, {
+      service,
+      status,
+      pathname,
+      message,
+    });
+
+    return { error: message, status };
+  }
+};
