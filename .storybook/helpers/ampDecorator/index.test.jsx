71b3d80c1636af85bdca667b4c7de9bcc3ae67fa
Karina Thomas <karina.thomas@bbc.co.uk>
2024-11-22 12:31:13 +0000

    Delete all test files

--- .storybook/helpers/ampDecorator/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AmpDecorator from './index';
-
-describe('AmpDecorator', () => {
-  it('should render correctly', () => {
-    const component = AmpDecorator(() => <div>Foobar</div>);
-    expect(component).toMatchSnapshot();
-  });
-});
