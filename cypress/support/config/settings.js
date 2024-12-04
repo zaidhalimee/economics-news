667f952536a29d1faf390dbbee8846d683d731ba
Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>
2024-12-04 15:46:35 +0000

    Enable onDemandAudio smoke tests for arabic & gahuza

--- cypress/support/config/settings.js
+++ cypress/support/config/settings.js
@@ -699,7 +699,7 @@ module.exports = () => ({
             enabled: true,
           },
         },
-        smoke: false,
+        smoke: true,
       },
       onDemandTV: { environments: undefined, smoke: false },
       topicPage: {
@@ -1588,7 +1588,7 @@ module.exports = () => ({
             enabled: true,
           },
         },
-        smoke: false,
+        smoke: true,
       },
       onDemandTV: { environments: undefined, smoke: false },
       topicPage: { environments: undefined, smoke: false },
