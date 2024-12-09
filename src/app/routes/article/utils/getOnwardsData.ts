929ec6096ed2e233a22a9fdb6001d0e244afa998
L0u155 <louis.matsika@bbc.co.uk>
2024-12-09 10:57:43 +0000

    getOnwardsData.ts - SECONDARY_DATA_TIMEOUT

--- src/app/routes/article/utils/getOnwardsData.ts
+++ src/app/routes/article/utils/getOnwardsData.ts
@@ -5,7 +5,7 @@ import isEmpty from 'ramda/src/isEmpty';
 import { DATA_FETCH_ERROR } from '#lib/logger.const';
 import getRecommendationsUrl from '#app/lib/utilities/getUrlHelpers/getRecommendationsUrl';
 import { SECONDARY_DATA_TIMEOUT } from '#app/lib/utilities/getFetchTimeouts';
-import fetchPageData from '../../utils/fetchPageData';
+import fetchPageData from '#app/routes/utils/fetchPageData';
 import nodeLogger from '../../../lib/logger.node';
 import hasArticleRecommendations from './hasArticleRecommendations';
 
