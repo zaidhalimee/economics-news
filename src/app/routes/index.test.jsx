6ae40a799127563625ab371ed8483bbe8dfaf679
L0u155 <louis.matsika@bbc.co.uk>
2024-12-10 15:21:04 +0000

    revert to last actual passing commit

--- src/app/routes/index.test.jsx
+++ src/app/routes/index.test.jsx
@@ -17,18 +17,17 @@ import onDemandTvPageJson from '#data/pashto/bbc_pashto_tv/tv_programmes/w13xttn
 import articlePageJson from '#data/persian/articles/c4vlle3q337o.json';
 import sportArticlePageJson from '#data/sport/judo/articles/cj80n66ddnko.json';
 import mediaAssetPageJson from '#data/yoruba/cpsAssets/media-23256797.json';
-import routes from '.';
 
 import { ERROR_PAGE, FRONT_PAGE } from '#app/routes/utils/pageTypes';
 import gahuzaOnDemandAudio from '#data/gahuza/bbc_gahuza_radio/p02pcb5c.json';
+import routes from '.';
 import {
   act,
   render,
   screen,
-} from '#app/components/react-testing-library-with-providers';
-import { suppressPropWarnings } from '#psammead/psammead-test-helpers/src';
+} from '../components/react-testing-library-with-providers';
+import { suppressPropWarnings } from '../legacy/psammead/psammead-test-helpers/src';
 import * as fetchDataFromBFF from './utils/fetchDataFromBFF';
-
 
 global.performance.getEntriesByName = jest.fn(() => []);
 
