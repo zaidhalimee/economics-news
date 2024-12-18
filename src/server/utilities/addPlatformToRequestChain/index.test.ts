782eb9f7c6e3787aa41a6f50199c1e5b78cf6ed7
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-12-18 16:48:19 +0000

    Updates unit test

--- src/server/utilities/addPlatformToRequestChain/index.test.ts
+++ src/server/utilities/addPlatformToRequestChain/index.test.ts
@@ -11,6 +11,16 @@ describe('addPlatformToRequestChain', () => {
     expect(result).toStrictEqual('SERVICE_1,SERVICE_2,SIMORGH,EXPRESS');
   });
 
+  it('appends NEXTJS to the req-svc-chain header if it exists', () => {
+    const headers = { 'req-svc-chain': 'SERVICE_1,SERVICE_2' };
+    const result = addPlatformToRequestChain({
+      headers,
+      application: 'NEXTJS',
+    });
+
+    expect(result).toStrictEqual('SERVICE_1,SERVICE_2,SIMORGH,NEXTJS');
+  });
+
   it('returns SIMORGH if the req-svc-chain header does not exist', () => {
     const headers = { 'header-name': 'header-value' };
     const result = addPlatformToRequestChain({
