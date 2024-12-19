2f69cc4f68b01aed9562cb6550fde2f1a3680541
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-12-19 11:28:56 +0000

    WSTEAM1-HACK: Update

--- src/app/components/EasyReadCTA/index.stories.tsx
+++ src/app/components/EasyReadCTA/index.stories.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { RequestContextProvider } from '#app/contexts/RequestContext';
 import EasyReadCTA from '.';
 
 export const Component = () => (
-  <EasyReadCTA easyReadAssetId="crkdy3r685jo" originalAssetId="cy0grkwd3zlo" />
+  <RequestContextProvider
+    pathname="/mundo/afrique/cdwrpl7qwqqo"
+    pageType="article"
+    service="afrique"
+  >
+    <EasyReadCTA
+      easyReadAssetId="crkdy3r685jo"
+      originalAssetId="cy0grkwd3zlo"
+    />
+  </RequestContextProvider>
 );
 
 export const EasyComponent = () => (
-  <EasyReadCTA originalAssetId="cy0grkwd3zlo" />
+  <RequestContextProvider
+    pathname="/mundo/afrique/cdwrpl7qwqqo"
+    pageType="article"
+    service="afrique"
+  >
+    <EasyReadCTA originalAssetId="cy0grkwd3zlo" />{' '}
+  </RequestContextProvider>
 );
 
 export default {
