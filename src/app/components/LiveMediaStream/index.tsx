e02d440173430fcfbdf9dc952c14ea64a52ef22d
L0u155 <louis.matsika@bbc.co.uk>
2024-12-23 12:08:00 +0000

    add pressable button

--- src/app/components/LiveMediaStream/index.tsx
+++ src/app/components/LiveMediaStream/index.tsx
@@ -1,13 +1,22 @@
 /** @jsx jsx */
-import React from 'react';
 import { jsx } from '@emotion/react';
+import { useState } from 'react';
 
 const LiveMediaStream = () => {
-  const testText = 'hello';
+  const testText = 'button is ';
+
+  const [showMedia, setShowMedia] = useState(false);
+
+  const handleClick = () => {
+    setShowMedia(!showMedia);
+  };
 
   return (
     <div>
-      <button type="button">{testText}</button>
+      <button type="button" onClick={handleClick}>
+        {testText}
+        {`${showMedia}`}
+      </button>
     </div>
   );
 };
