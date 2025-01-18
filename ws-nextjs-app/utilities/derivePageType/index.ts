911722c522149a089d321ecc7f138171d2fe36f7
Aaron Moore <aaron.moore@bbc.co.uk>
2025-01-18 11:05:40 +0000

    Error page type value

--- ws-nextjs-app/utilities/derivePageType/index.ts
+++ ws-nextjs-app/utilities/derivePageType/index.ts
@@ -1,22 +1,27 @@
 import { PageTypes } from '#app/models/types/global';
 import {
+  ARTICLE_PAGE,
   AV_EMBEDS,
   DOWNLOADS_PAGE,
   LIVE_PAGE,
   UGC_PAGE,
 } from '#app/routes/utils/pageTypes';
+import {
+  isOptimoIdCheck,
+  isCpsIdCheck,
+} from '#app/routes/utils/constructPageFetchUrl';
 
 export default function derivePageType(
   pathname: string,
 ): PageTypes | 'Unknown' {
-  const sanitisedPathname = new URL(pathname, 'http://bbc.com')?.pathname;
-
-  if (!sanitisedPathname) return 'Unknown';
+  const sanitisedPathname = new URL(pathname, 'http://bbc.com').pathname;
 
   if (sanitisedPathname.includes('live')) return LIVE_PAGE;
   if (sanitisedPathname.includes('send')) return UGC_PAGE;
   if (sanitisedPathname.includes('av-embeds')) return AV_EMBEDS;
   if (sanitisedPathname.includes('downloads')) return DOWNLOADS_PAGE;
+  if (isOptimoIdCheck(sanitisedPathname)) return ARTICLE_PAGE;
+  if (isCpsIdCheck(sanitisedPathname)) return ARTICLE_PAGE;
 
   return 'Unknown';
 }
