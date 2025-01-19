6086b48563fcb7a2fbf1ca24576e232cd0c972a7
Aaron Moore <aaron.moore@bbc.co.uk>
2025-01-19 11:46:10 +0000

    Test updates for `constructPageFetchUrl`

--- src/app/routes/utils/constructPageFetchUrl/index.ts
+++ src/app/routes/utils/constructPageFetchUrl/index.ts
@@ -227,18 +227,19 @@ const constructPageFetchUrl = ({
       case ARTICLE_PAGE: {
         const host = `http://${process.env.HOSTNAME || 'localhost'}`;
         const port = process.env.PORT ? `:${process.env.PORT}` : '';
-        const { assetId } = parseAvRoute(pathname);
 
         if (isOptimoIdCheck(pathname)) {
           fetchUrl = Url(
-            `${host}${port}/api/local/${service}/articles/${assetId}${variant ? `/${variant}` : ''}`,
+            `${host}${port}/api/local/${service}/articles/${id}${variant ? `/${variant}` : ''}`,
           );
+          break;
         }
 
         if (isCpsIdCheck(pathname)) {
           fetchUrl = Url(
-            `${host}${port}/api/local/${service}/cpsAssets/${variant ? `${variant}/` : ''}${assetId}`,
+            `${host}${port}/api/local/${service}/cpsAssets/${variant ? `${variant}/` : ''}${id}`,
           );
+          break;
         }
 
         break;
