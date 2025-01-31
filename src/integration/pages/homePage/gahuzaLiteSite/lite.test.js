86475a6b00a0fb38f36676933a9592de71582bef
Karina Thomas <karina.thomas@bbc.co.uk>
2025-01-31 14:21:44 +0000

    Adding an integration for the gahuza lite homepage

---
+++ src/integration/pages/homePage/gahuzaLiteSite/lite.test.js
@@ -0,0 +1,12 @@
+/**
+ * @service gahuza
+ * @pathname /gahuza
+ */
+
+import { runLiteSiteTests } from '../../../common';
+
+describe('Lite Site', () => {
+  describe(pageType, () => {
+    runLiteSiteTests();
+  });
+});
