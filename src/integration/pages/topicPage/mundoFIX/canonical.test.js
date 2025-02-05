dbe06b58c0e7b0ad656b6845d63e7884320a9b60
Karina Thomas <karina.thomas@bbc.co.uk>
2025-02-05 20:48:01 +0000

    Integration test against a FIX which has been converted to a Topic Page

---
+++ src/integration/pages/topicPage/mundoFIX/canonical.test.js
@@ -0,0 +1,12 @@
+/**
+ * @service mundo
+ * @pathname /mundo/noticias-internacional-53826365
+ */
+
+import runCanonicalTests from '../canonicalTests';
+
+describe('Canonical', () => {
+  describe(pageType, () => {
+    runCanonicalTests(service);
+  });
+});
