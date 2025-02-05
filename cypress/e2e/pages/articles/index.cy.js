314a2142091cea966e08eab43bb322e500e92a42
Karina Thomas <karina.thomas@bbc.co.uk>
2025-02-05 13:19:01 +0000

    Include all amp and canonical tests in the article tests

--- cypress/e2e/pages/articles/index.cy.js
+++ cypress/e2e/pages/articles/index.cy.js
@@ -1,14 +1,25 @@
 /* eslint-disable import/no-relative-packages */
 import runTestsForPage from '#nextjs/cypress/support/helpers/runTestsForPage';
-import { testsThatAlwaysRunForAllPages } from '../testsForAllPages';
-import crossPlatformTests from './tests';
-import ampTests from './testsForAMPOnly';
-import canonicalTests from './testsForCanonicalOnly';
-import liteTests from './testsForLiteOnly';
+import { testsThatAlwaysRunForAllPages as testsForAllPages } from '../testsForAllPages';
+import { testsThatFollowSmokeTestConfigForAllCanonicalPages as testsForAllCanonicalPages } from '../testsForAllCanonicalPages';
+import { testsThatFollowSmokeTestConfigForAllAMPPages as testsForAllAMPPages } from '../testsForAllAMPPages';
+import canonicalAndAmpArticleTests from './tests';
+import ampArticleTests from './testsForAMPOnly';
+import canonicalArticleTests from './testsForCanonicalOnly';
+import liteArticleTests from './testsForLiteOnly';
 
-const testsForAllPagesCrossPlatform = [
-  testsThatAlwaysRunForAllPages,
-  crossPlatformTests,
+const canonicalTests = [
+  testsForAllPages,
+  testsForAllCanonicalPages,
+  canonicalAndAmpArticleTests,
+  canonicalArticleTests,
+];
+
+const ampTests = [
+  testsForAllPages,
+  testsForAllAMPPages,
+  canonicalAndAmpArticleTests,
+  ampArticleTests,
 ];
 
 const smokeCanonicalTestSuites = [
@@ -16,101 +27,101 @@ const smokeCanonicalTestSuites = [
     path: '/gahuza/articles/c5y51yxeg53o',
     runforEnv: ['local'],
     service: 'gahuza',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/mundo/articles/ce42wzqr2mko',
     runforEnv: ['local', 'test'],
     service: 'mundo',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/mundo/articles/ce7p1pw7165o',
     runforEnv: ['live'],
     service: 'mundo',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/news/articles/cn7k01xp8kxo',
-    runforEnv: ['local'],
+    runforEnv: ['local', 'test'],
     service: 'news',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/persian/articles/cej3lzd5e0go',
     runforEnv: ['local', 'test'],
     service: 'persian',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/persian/articles/cld9872jgyjo',
     runforEnv: ['live'],
     service: 'persian',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/persian/articles/crgxnrdl1xvo',
     runforEnv: ['live'],
     service: 'persian',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/pidgin/articles/cgwk9w4zlg8o',
     runforEnv: ['live'],
     service: 'pidgin',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/pidgin/articles/crrrkxz2k0ko',
     runforEnv: ['test'],
     service: 'pidgin',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/pidgin/articles/cwl08rd38l6o',
     runforEnv: ['local', 'test'],
     service: 'pidgin',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/pidgin/articles/cw8qv1d11l9o',
     runforEnv: ['live'],
     service: 'pidgin',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/scotland/articles/czwj5l0n210o',
     runforEnv: ['local'],
     service: 'scotland',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/serbian/articles/c805k05kr73o/cyr',
     runforEnv: ['local'],
     service: 'serbian',
     variant: 'cyr',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/serbian/articles/c805k05kr73o/lat',
     runforEnv: ['local'],
     service: 'serbian',
     variant: 'lat',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/zhongwen/articles/c3xd4x9prgyo/simp',
     runforEnv: ['local'],
     service: 'zhongwen',
     variant: 'simp',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/zhongwen/articles/c3xd4x9prgyo/trad',
     runforEnv: ['local'],
     service: 'zhongwen',
     variant: 'trad',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
 ];
 
@@ -119,68 +130,68 @@ const nonSmokeCanonicalTestSuites = [
     path: '/afaanoromoo/articles/c4g19kgl85ko',
     runforEnv: ['test'],
     service: 'afaanoromoo',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/afrique/articles/cz216x22106o',
     runforEnv: ['test'],
     service: 'afrique',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/azeri/articles/cv0lm08kngmo',
     runforEnv: ['live'],
     service: 'azeri',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/japanese/articles/cdd6p3r9g7jo',
     runforEnv: ['test'],
     service: 'japanese',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/japanese/articles/cj4m7n5nrd8o',
     runforEnv: ['live'],
     service: 'japanese',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/kyrgyz/articles/c414v42gy75o',
     runforEnv: ['live'],
     service: 'kyrgyz',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/kyrgyz/articles/c41knv20gk7o',
     runforEnv: ['live'],
     service: 'kyrgyz',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/kyrgyz/articles/cpgx4k72wv4o',
     runforEnv: ['live'],
     service: 'kyrgyz',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/mundo/articles/ce7p1pw7165o',
     runforEnv: ['live'],
     service: 'mundo',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
 
   {
     path: '/nepali/articles/c16ljg1v008o',
     runforEnv: ['live'],
     service: 'nepali',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
   {
     path: '/ukrainian/articles/c8zv0eed9gko',
     runforEnv: ['live'],
     service: 'ukrainian',
-    tests: [...testsForAllPagesCrossPlatform, canonicalTests],
+    tests: [...canonicalTests],
   },
 ];
 
@@ -190,38 +201,32 @@ const nonSmokeCanonicalTestSuites = [
  */
 let canonicalTestSuites = [
   ...nonSmokeCanonicalTestSuites,
-  smokeCanonicalTestSuites,
+  ...smokeCanonicalTestSuites,
 ];
 
 if (Cypress.env.SMOKE) {
   canonicalTestSuites = smokeCanonicalTestSuites;
 }
 
-const ampTestSuites = canonicalTestSuites
-  .map(testSuite => {
-    return {
-      ...testSuite,
-      path: `${testSuite.path}.amp`,
-      tests: [...testsForAllPagesCrossPlatform, ampTests],
-    };
-  })
-  // Additional scenarios for news on higher environments
-  .push(
-    ...[
-      {
-        path: '/news/articles/cn7k01xp8kxo.amp',
-        runforEnv: ['test', 'live'],
-        service: 'news',
-        tests: [...testsForAllPagesCrossPlatform, canonicalTests],
-      },
-      {
-        path: '/news/articles/cj7xrxz0e8zo.amp',
-        runforEnv: ['live'],
-        service: 'news',
-        tests: [...testsForAllPagesCrossPlatform, canonicalTests],
-      },
-    ],
-  );
+const ampTestSuites = canonicalTestSuites.map(testSuite => {
+  return {
+    ...testSuite,
+    path: `${testSuite.path}.amp`,
+    tests: [...ampTests],
+  };
+});
+
+// Additional scenarios for news on higher environments
+ampTestSuites.push(
+  ...[
+    {
+      path: '/news/articles/cj7xrxz0e8zo.amp',
+      runforEnv: ['live'],
+      service: 'news',
+      tests: [...ampTests],
+    },
+  ],
+);
 
 const liteTestSuites = canonicalTestSuites
   .filter(({ service }) => service !== 'news')
@@ -229,7 +234,7 @@ const liteTestSuites = canonicalTestSuites
     return {
       ...testSuite,
       path: `${testSuite.path}.lite`,
-      tests: [liteTests],
+      tests: [liteArticleTests],
     };
   });
 
