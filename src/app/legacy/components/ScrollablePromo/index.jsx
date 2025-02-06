3fb1c23cb06895f926b78eaa98f6b746503d7d42
chrish01 <holly.christopher@bbc.co.uk>
2025-02-06 16:43:26 +0000

    adds scroll

--- src/app/legacy/components/ScrollablePromo/index.jsx
+++ src/app/legacy/components/ScrollablePromo/index.jsx
@@ -41,6 +41,7 @@ const ScrollablePromoContainer = styled.div`
   background: #f6f6f6;
   padding: ${GEL_SPACING};
   display: flex;
+  overflow: scroll;
   ${({ experimentVariant }) =>
     experimentVariant &&
     `
