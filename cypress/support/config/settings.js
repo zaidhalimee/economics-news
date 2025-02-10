b0b36f29de50251dd056a960f93f7377fd95aa89
Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>
2025-02-10 17:22:22 +0000

    Enable pidgin, arabic & serbian lat topic page smoke tests on local

--- cypress/support/config/settings.js
+++ cypress/support/config/settings.js
@@ -645,8 +645,8 @@ module.exports = () => ({
             enabled: true,
           },
           local: {
-            paths: [],
-            enabled: false,
+            paths: ['/arabic/topics/cwr9j7nv58nt'],
+            enabled: true,
           },
         },
         smoke: true,
@@ -4271,8 +4271,8 @@ module.exports = () => ({
             enabled: true,
           },
           local: {
-            paths: ['/pidgin/topics/cnq68qvkjp1t'], // DO NOT ENABLE
-            enabled: false,
+            paths: ['/pidgin/topics/c95y35941vrt'],
+            enabled: true,
           },
         },
         smoke: true,
@@ -5088,19 +5088,19 @@ module.exports = () => ({
       topicPage: {
         environments: {
           live: {
-            paths: ['/serbian/lat/topics/c1gd303q6y6t'],
+            paths: ['/serbian/topics/c1gd303q6y6t/lat'],
             enabled: true,
           },
           test: {
-            paths: ['/serbian/lat/topics/c1gd303q6y6t'],
+            paths: ['/serbian/topics/c1gd303q6y6t/lat'],
             enabled: true,
           },
           local: {
-            paths: [''],
-            enabled: false,
+            paths: ['/serbian/topics/c1gd303q6y6t/lat'],
+            enabled: true,
           },
         },
-        smoke: false,
+        smoke: true,
       },
       mediaAssetPage: {
         environments: {
