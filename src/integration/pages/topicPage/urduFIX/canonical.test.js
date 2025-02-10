ef1faf1e846d53c30fcd9ef3215259e2449bd90c
Karina Thomas <karina.thomas@bbc.co.uk>
2025-02-10 12:55:40 +0000

    Reinstate test for urdu feature index page, for comparison with previous implementation

---
+++ src/integration/pages/topicPage/urduFIX/canonical.test.js
@@ -0,0 +1,12 @@
+/**
+ * @service urdu
+ * @pathname /urdu/science-51314202
+ */
+
+import runCanonicalTests from '../canonicalTests';
+
+describe('Canonical', () => {
+  describe(pageType, () => {
+    runCanonicalTests(service);
+  });
+});
