35bd0cc433052eb76faf86ba683cdd250edfbc86
Karina Thomas <karina.thomas@bbc.co.uk>
2025-02-10 12:39:00 +0000

    Use the same afrique fixture so that the snapshot files can be compared

---
+++ src/integration/pages/topicPage/afriqueFIX/canonical.test.js
@@ -0,0 +1,12 @@
+/**
+ * @service afrique
+ * @pathname /afrique/48465371
+ */
+
+import runCanonicalTests from '../canonicalTests';
+
+describe('Canonical', () => {
+  describe(pageType, () => {
+    runCanonicalTests(service);
+  });
+});
