696f2585a121f12130278bf495f0790022c91a6a
Karina Thomas <karina.thomas@bbc.co.uk>
2025-02-11 18:14:34 +0000

    Ensure referrer is sent client side on opera mini

--- src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript/index.ts
+++ src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript/index.ts
@@ -1,14 +1,20 @@
 import isOperaProxy from '#app/lib/utilities/isOperaProxy';
+import { getReferrer } from '#app/lib/analyticsUtils';
 
-const sendBeaconOperaMiniScript = (atiPageViewUrlString: string) => `
+const sendBeaconOperaMiniScript = (atiPageViewUrlString: string) => {
+  const referrer = getReferrer();
+  const referrerParam = referrer ? `&ref=${referrer}` : '';
+
+  return `
     if (${isOperaProxy.toString()}() && !Boolean(window.hasOperaMiniScriptRan)) {
       window.hasOperaMiniScriptRan = true;
 
       var xhr = new XMLHttpRequest();
-      xhr.open("GET", "${atiPageViewUrlString}", true);
+      xhr.open("GET", "${atiPageViewUrlString}${referrerParam}", true);
       xhr.withCredentials = true;
       xhr.send();
     }
 `;
+};
 
 export default sendBeaconOperaMiniScript;
