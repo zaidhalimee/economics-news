6418e2eaab21d948efbe2535d292522eacc7bdcf
L0u155 <louis.matsika@bbc.co.uk>
2025-02-12 15:58:04 +0000

    revert last commit

--- src/app/components/Curation/CurationPromo/index.styles.ts
+++ src/app/components/Curation/CurationPromo/index.styles.ts
@@ -1,12 +1,11 @@
 import { css, Theme } from '@emotion/react';
-import pixelsToRem from '#app/utilities/pixelsToRem';
 
 export default {
   promo: ({ isLite, spacings, mq }: Theme) =>
     css({
       ...(isLite && {
         [mq.GROUP_2_MAX_WIDTH]: {
-          marginTop: `${pixelsToRem(48)}rem`,
+          marginTop: `${spacings.SEXTUPLE}rem`,
         },
       }),
     }),
