fb9a9e0b1af6596323fe405d257e523f7420c766
mattbbc <matthew.laughlin@bbc.co.uk>
2025-02-18 12:20:10 +0000

    Fix margins

--- src/app/legacy/components/ScrollablePromo/index.jsx
+++ src/app/legacy/components/ScrollablePromo/index.jsx
@@ -62,8 +62,18 @@ const ScrollablePromoContainer = styled.div`
     @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
       display: none;
     }
+
+    margin: 0rem;
+    @media (min-width: ${GEL_GROUP_0_SCREEN_WIDTH_MIN}) {
+      margin: 0rem;
+    }
+    @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+      margin: 0 -0.2rem;
+    }
+    @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+      margin: 0 -0.8rem;
+    }
     width: 100vw;
-    margin-left: calc(-50vw + 50%);
   `}
 `;
 
@@ -105,13 +115,13 @@ const LabelComponent = styled(({ ariaLabel, experimentVariant, ...props }) => (
 
     margin: 0rem;
     @media (min-width: ${GEL_GROUP_0_SCREEN_WIDTH_MIN}) {
-      margin-left: 0rem;
+      margin: 0rem;
     }
     @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
-      margin-left: -0.2rem;
+      margin: 0 -0.2rem;
     }
     @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
-      margin-left: -0.8rem;
+      margin: 0 -0.8rem;
     }
 
     @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}){
