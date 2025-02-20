805121ce77f8f5398b16cddd2209914e663f495c
eagerterrier <eagerterrier@gmail.com>
2025-02-20 16:39:06 +0000

    INI: add topic collector

--- src/app/components/PageLayoutWrapper/index.tsx
+++ src/app/components/PageLayoutWrapper/index.tsx
@@ -140,6 +140,8 @@ const PageLayoutWrapper = ({
                 let wrappedMonth = wrappedPageTimeStart.getMonth() + 1;
                 let wrappedStorageKey = 'ws_bbc_wrapped';
                 let wrappedContents = {};
+                let topicsStorageKey = 'ws_bbc_topics';
+                let topicsContents = localStorage.getItem(topicsStorageKey) || {};
                 wrappedContents[wrappedYear] = {
                     'byMonth': {},
                     'pageTypeCounts': {},
@@ -165,7 +167,18 @@ const PageLayoutWrapper = ({
                   pageData?.metadata?.topics,
                 )};
                 if (wrappedTopics) {
-                    wrappedTopics.forEach(({ topicName }) => {
+                    wrappedTopics.forEach(({ topicName, topicId }) => {
+                        if (!topicsContents.${service}) topicsContents.${service} = {};
+                        if (topicsContents.${service}[topicName]) {
+                            topicsContents.${service}[topicName].count++;
+                        }
+                        else {
+                            topicsContents.${service}[topicName] = {
+                                'count': 1,
+                                'id': topicId,
+                                'path': "/${service}/topics/" + topicId
+                            };
+                        }
                         wrappedContentsShortcut.topicCounts[topicName] = wrappedContentsShortcut.topicCounts[topicName] ? wrappedContentsShortcut.topicCounts[topicName] + 1 : 1;
                     });
                 }
@@ -185,6 +198,7 @@ const PageLayoutWrapper = ({
                 wrappedContentsShortcut.pageTypeCounts.${reportingPageType} = wrappedContentsShortcut.pageTypeCounts.${reportingPageType} ? wrappedContentsShortcut.pageTypeCounts.${reportingPageType} + 1 : 1;
                 wrappedContentsShortcut.byMonth[wrappedMonth] = wrappedContentsShortcut.byMonth[wrappedMonth] ? wrappedContentsShortcut.byMonth[wrappedMonth] + 1 : 1;
                 wrappedContents[wrappedYear] = wrappedContentsShortcut;
+                localStorage.setItem(topicsStorageKey, JSON.stringify(topicsContents));
     `;
 
   return (
