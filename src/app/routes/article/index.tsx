ece30257f55f83e2a38850feb960b62ffe21a77e
Pedram Valiani <pedram.valiani@bbc.co.uk>
2025-02-20 17:28:16 +0000

    prettier

--- src/app/routes/article/index.tsx
+++ src/app/routes/article/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  ArticlePage,
-  ErrorPage,
-  MediaArticlePage,
-} from '#pages/index';
+import { ArticlePage, ErrorPage, MediaArticlePage } from '#pages/index';
 import {
   articlePath,
   cpsAssetPagePath,
