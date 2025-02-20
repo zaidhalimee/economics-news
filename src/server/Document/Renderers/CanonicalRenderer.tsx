a7cd50fd427e2bce90f51bf20df225d5d3905008
Nabeel Khan <nabeel.khan@bbc.co.uk>
2025-02-20 14:19:34 +0000

    removed redundant code replaced with component

--- src/server/Document/Renderers/CanonicalRenderer.tsx
+++ src/server/Document/Renderers/CanonicalRenderer.tsx
@@ -40,20 +40,6 @@ export default function CanonicalRenderer({
     <html lang="en-GB" className={NO_JS_CLASSNAME} {...htmlAttrs}>
       <head>
         <ReverbTemplate />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.__reverb = {};
-            window.__reverb.__reverbLoadedPromise = new Promise((resolve, reject) => {
-              window.__reverb.__resolveReverbLoaded = resolve;
-              window.__reverb.__rejectReverbLoaded = reject;
-            });
-
-            window.__reverb.__reverbTimeout = setTimeout(() => {
-              window.__reverb.__rejectReverbLoaded();
-            }, 5000);`,
-          }}
-        />
         <script async src={`${getEnvConfig().SIMORGH_REVERB_SOURCE}`} />
 
         {isApp && <meta name="robots" content="noindex" />}
