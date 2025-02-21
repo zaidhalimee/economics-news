ae95033d187971ec1dcf346fd361313f6fdaf37d
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-02-21 11:41:02 +0000

    adds privacy banner toggle

--- src/app/legacy/containers/ConsentBanner/index.canonical.jsx
+++ src/app/legacy/containers/ConsentBanner/index.canonical.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 
 import { UserContext } from '#contexts/UserContext';
+import useToggle from '#hooks/useToggle';
 import Banner from './Banner/index.canonical';
 import useConsentBanners from './useConsentBanners';
 import { RequestContext } from '../../../contexts/RequestContext';
@@ -8,6 +9,7 @@ import { RequestContext } from '../../../contexts/RequestContext';
 const Canonical = ({ onDismissFocusRef = null }) => {
   const { updateCookiePolicy } = useContext(UserContext);
   const { isUK, showCookieBannerBasedOnCountry } = useContext(RequestContext);
+  const { enabled } = useToggle('privacyPolicy');
 
   const {
     showPrivacyBanner,
@@ -19,7 +21,7 @@ const Canonical = ({ onDismissFocusRef = null }) => {
 
   return (
     <>
-      {showPrivacyBanner && (
+      {showPrivacyBanner && enabled && (
         <Banner type="privacy" onAccept={handlePrivacyBannerAccepted} />
       )}
       {showCookieBanner && (
