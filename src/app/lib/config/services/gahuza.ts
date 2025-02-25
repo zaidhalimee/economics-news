9538eaf46a6980c13ac434c6351158eee4fbe4ec
louisearchibald <louise.archibald@bbc.co.uk>
2025-02-25 09:48:12 +0000

    enable reverb on batch 1 services

--- src/app/lib/config/services/gahuza.ts
+++ src/app/lib/config/services/gahuza.ts
@@ -13,6 +13,7 @@ export const service: DefaultServiceConfig = {
     atiAnalyticsAppName: 'news-gahuza',
     atiAnalyticsProducerId: '40',
     atiAnalyticsProducerName: 'GAHUZA',
+    useReverb: true,
     chartbeatDomain: 'gahuza.bbc.co.uk',
     brandName: 'BBC News Gahuza',
     product: 'BBC News',
