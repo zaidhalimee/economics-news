9763a3cf74f76ad41801ff6b5836343c201d8eba
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2025-02-27 13:36:38 +0000

    WORLDSERVICE-379: UpdatE

--- src/server/utilities/liteATITracking/viewTracking/index.ts
+++ src/server/utilities/liteATITracking/viewTracking/index.ts
@@ -1,5 +1,12 @@
-export default () => {
+export default async () => {
+  if (typeof window.IntersectionObserver === 'undefined') {
+    console.log('OBSERVER POLYFILL ME OUT!!!!');
+    // Polyfill IntersectionObserver, e.g. for IE11
+    await import('intersection-observer');
+  }
+
   const observer = new IntersectionObserver(entries => {
+    console.log('CHECK ME OUT!!!!');
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         const { target } = entry;
