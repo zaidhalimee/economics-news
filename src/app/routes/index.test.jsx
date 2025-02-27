cf4a60f38284aa7ff4bf707f9230da7f7c33f3a5
Aaron Moore <aaron.moore@bbc.co.uk>
2025-02-27 14:54:43 +0000

    Component removals

--- src/app/routes/index.test.jsx
+++ src/app/routes/index.test.jsx
@@ -17,7 +17,7 @@ import articlePageJson from '#data/persian/articles/c4vlle3q337o.json';
 import sportArticlePageJson from '#data/sport/judo/articles/cj80n66ddnko.json';
 import mediaAssetPageJson from '#data/yoruba/cpsAssets/media-23256797.json';
 
-import { ERROR_PAGE, FRONT_PAGE } from '#app/routes/utils/pageTypes';
+import { ERROR_PAGE, HOME_PAGE } from '#app/routes/utils/pageTypes';
 import * as fetchDataFromBFF from '#app/routes/utils/fetchDataFromBFF';
 import gahuzaOnDemandAudio from '#data/gahuza/bbc_gahuza_radio/p02pcb5c.json';
 // eslint-disable-next-line import/order
@@ -348,7 +348,7 @@ describe('Routes', () => {
       });
       await renderRouter({
         pathname,
-        pageType: FRONT_PAGE,
+        pageType: HOME_PAGE,
         service: 'afrique',
         error: {
           message: error,
