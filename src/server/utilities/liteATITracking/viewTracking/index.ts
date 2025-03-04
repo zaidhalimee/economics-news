4d6cc15e4e1c89469413643c80d40f230147c0e5
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2025-03-04 15:29:05 +0000

    WORLDSERVICE-379: Update

--- src/server/utilities/liteATITracking/viewTracking/index.ts
+++ src/server/utilities/liteATITracking/viewTracking/index.ts
@@ -3,22 +3,31 @@ export default () => {
   const VIEWED_DURATION_MS = 1000;
 
   const options = { threshold: MIN_VIEWED_PERCENT };
+
   const observer = new IntersectionObserver(entries => {
     console.log('INTERSECTION OBSERVER LOADED');
-    entries.forEach(entry => {
+    for (let i = 0; i < entries.length; i += 1) {
+      const entry = entries[i];
       if (entry.isIntersecting) {
         console.log('IS INTERSECTING');
         const { target } = entry;
         const atiURL = target.getAttribute('data-lite-ati-view-tracking');
-        setTimeout(() => {
-          console.log('SENDING INTERSECTION');
-          window.processClientDeviceAndSendLite(atiURL as string);
-          observer.unobserve(target);
-        }, VIEWED_DURATION_MS);
+
+        const startTime = new Date().getTime();
+        let currTime = startTime;
+        while (currTime - startTime < VIEWED_DURATION_MS) {
+          currTime = new Date().getTime();
+        }
+
+        console.log('SENDING INTERSECTION');
+        window.processClientDeviceAndSendLite(atiURL as string);
+        observer.unobserve(target);
       }
-    });
+    }
   }, options);
 
   const targets = document.querySelectorAll('[data-lite-ati-view-tracking]');
-  targets.forEach(target => observer.observe(target));
+  for (let i = 0; i < targets.length; i += 1) {
+    observer.observe(targets[i]);
+  }
 };
