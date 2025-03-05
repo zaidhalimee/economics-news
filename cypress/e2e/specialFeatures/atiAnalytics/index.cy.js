caa0250d2fd5ff581b6f4f25a72542cbb7b5867b
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-05 11:28:28 +0000

    Add E2E tests for related topics on lite article pages

--- cypress/e2e/specialFeatures/atiAnalytics/index.cy.js
+++ cypress/e2e/specialFeatures/atiAnalytics/index.cy.js
@@ -266,6 +266,7 @@ const liteTestSuites = canonicalTestSuites
     switch (testSuite.contentType) {
       case 'article':
         liteSiteTests.push(assertLiteSiteCTAComponentClick);
+        liteSiteTests.push(assertRelatedTopicsComponentView);
         break;
       case 'index-home':
         liteSiteTests.push(assertMostReadComponentClick);
