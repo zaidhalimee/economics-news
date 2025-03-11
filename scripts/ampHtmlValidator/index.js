27d06c5c13f4f158c967c705e477c9d906a6f8c4
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-11 10:50:17 +0000

    Add test title

--- scripts/ampHtmlValidator/index.js
+++ scripts/ampHtmlValidator/index.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 /* eslint-disable import/extensions */
 import checkManifest from './checkManifest/index.js';
 import { runValidator } from './validator/index.js';
 
-runValidator();
+console.log(`\nAMP Validation`);
+await runValidator();
 
+console.log(`\nAMP Manifest File`);
 await checkManifest();
