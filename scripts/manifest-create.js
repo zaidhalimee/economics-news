438a73ca53714ed630d17ba4e4b7bd809285a28e
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-12 12:48:15 +0000

    Update scope & add 96x96 icon for the lite site shortcut

--- scripts/manifest-create.js
+++ scripts/manifest-create.js
@@ -1,39 +1,80 @@
 const fs = require('fs');
-const services = ["afaanoromoo", "afrique", "amharic", "arabic", "archive", "azeri", "bengali", "burmese", "cymrufyw", "gahuza", "gujarati", "hausa", "hindi", "igbo", "indonesia", "japanese", "korean", "kyrgyz", "marathi", "mundo", "naidheachdan", "nepali", "news", "newsround", "pashto", "persian", "pidgin", "portuguese", "punjabi", "russian", "scotland", "serbian", "sinhala", "somali", "sport", "swahili", "tamil", "telugu", "thai", "tigrinya", "turkce", "ukchina", "ukrainian", "urdu", "uzbek", "vietnamese", "yoruba", "zhongwen"];
+const services = [
+  'afaanoromoo',
+  'afrique',
+  'amharic',
+  'arabic',
+  'azeri',
+  'bengali',
+  'burmese',
+  'gahuza',
+  'gujarati',
+  'hausa',
+  'hindi',
+  'igbo',
+  'indonesia',
+  'japanese',
+  'korean',
+  'kyrgyz',
+  'marathi',
+  'mundo',
+  'nepali',
+  'pashto',
+  'persian',
+  'pidgin',
+  'portuguese',
+  'punjabi',
+  'russian',
+  'serbian',
+  'sinhala',
+  'somali',
+  'swahili',
+  'tamil',
+  'telugu',
+  'thai',
+  'tigrinya',
+  'turkce',
+  'ukchina',
+  'ukrainian',
+  'urdu',
+  'uzbek',
+  'vietnamese',
+  'yoruba',
+  'zhongwen',
+];
 
 const IMAGE_VERSION = '1';
 const COPY_IMAGES_ACROSS = false;
 const NEW_IMAGES_FOLDER_LOCATION = '/absolute/path/to/your/new/images';
 
-const servicesWithoutLite = ["archive", "cymrufyw", "naidheachdan", "news", "newsround", "scotland", "sport"];
-
 services.forEach(service => {
-    const mainfestPath = `../public/${service}/manifest.json`;
-    const currentManifest = JSON.parse(fs.readFileSync(mainfestPath, 'utf-8'));
-    currentManifest.start_url = `/${service}`;
-    currentManifest.short_name = currentManifest.short_name.replace(' News', '');
-    if (!servicesWithoutLite.includes(service)) currentManifest.shortcuts = [
-        {
-          "name": "Lite Site",
-          "short_name": "Lite",
-          "url": `${currentManifest.start_url}.lite`
-        }
-    ];
-    currentManifest.scope = `/${service}`;
-    currentManifest.display = 'standalone';
-    currentManifest.icons = currentManifest.icons.map(icon => {
-        icon.src = `${icon.src.split('?')[0]}?v=${IMAGE_VERSION}`;
-        return icon;
-    });
-    if (COPY_IMAGES_ACROSS && !servicesWithoutLite.includes(service)) {
-        const iconsPathStem = `../public/${service}/images/icons/`;
-        const newIconsPathStem = `${NEW_IMAGES_FOLDER_LOCATION}/${service}/`;
-        currentManifest.icons.forEach((icon, i) => {
-            const imagename = icon.src.split('/').at(-1).split('?')[0];
-            fs.renameSync(newIconsPathStem + imagename, iconsPathStem + imagename);
-        });
-    }
-    newManifest = JSON.stringify(currentManifest, null, 2);
-    fs.writeFileSync(mainfestPath, newManifest);
+  const manifestPath = `../public/${service}/manifest.json`;
+  const currentManifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'));
+  currentManifest.start_url = `/${service}`;
+  currentManifest.id = 'delete';
+  currentManifest.short_name = currentManifest.short_name.replace(' News', '');
+  currentManifest.display = 'standalone';
+  currentManifest.icons = currentManifest.icons.map(icon => {
+    icon.src = `${icon.src.split('?')[0]}?v=${IMAGE_VERSION}`;
+    return icon;
+  });
+  currentManifest.shortcuts = [
+    {
+      name: 'Lite Site',
+      short_name: 'Lite',
+      url: `${currentManifest.start_url}.lite`,
+      icons: currentManifest.icons.filter(({ sizes }) => sizes === '96x96'),
+    },
+  ];
 
+  if (COPY_IMAGES_ACROSS) {
+    const iconsPathStem = `../public/${service}/images/icons/`;
+    const newIconsPathStem = `${NEW_IMAGES_FOLDER_LOCATION}/${service}/`;
+    currentManifest.icons.forEach((icon, i) => {
+      const imagename = icon.src.split('/').at(-1).split('?')[0];
+      fs.renameSync(newIconsPathStem + imagename, iconsPathStem + imagename);
+    });
+  }
+  newManifest = JSON.stringify(currentManifest, null, 2);
+  fs.writeFileSync(manifestPath, newManifest);
 });
