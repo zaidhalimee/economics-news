f23d3f709c387c6f3d221176909c393bbb5f79f5
L0u155 <louis.matsika@bbc.co.uk>
2025-03-13 16:07:19 +0000

    start on integration tests

---
+++ src/integration/pages/articles/canonicalLiteSiteCTA.js
@@ -0,0 +1,8 @@
+export default (pathname) => {
+  describe('canonical Lite Site CTA', () => {
+    it('should have an inline link', () => {
+      const liteSiteCTA = document.querySelector(`a[href=${pathname}.lite]`);
+      expect(liteSiteCTA).toBeInTheDocument();
+    });
+  });
+};
---
+++ src/integration/pages/articles/gahuza/canonical.test.js
@@ -0,0 +1,12 @@
+/**
+ * @service gahuza
+ * @pathname gahuza/articles/c5y51yxeg53o
+ */
+
+import canonicalLiteSiteCTA from "../canonicalLiteSiteCTA";
+
+describe('Canonical', () => {
+  describe(pageType, () => {
+    canonicalLiteSiteCTA('gahuza/articles/c5y51yxeg53o');
+  });
+});
