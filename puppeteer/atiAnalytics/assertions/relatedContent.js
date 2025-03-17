108a39566af5de4c2c882d1adb12f936187739ce
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-17 12:33:14 +0000

    Migrate relatedContent assertions to puppeteer

--- puppeteer/atiAnalytics/assertions/relatedContent.js
+++ puppeteer/atiAnalytics/assertions/relatedContent.js
@@ -1,4 +1,4 @@
-import { interceptATIAnalyticsBeacons, COMPONENTS } from '../helpers';
+import { COMPONENTS, scrollIntoView, click } from '../helpers';
 import { assertATIComponentClickEvent, assertATIComponentViewEvent } from '.';
 
 const { RELATED_CONTENT } = COMPONENTS;
@@ -8,21 +8,14 @@ export const assertRelatedContentComponentView = ({
   contentType,
   useReverb,
 }) => {
-  it('should send a view event for the Related Content component', () => {
-    cy.url().then(url => {
-      interceptATIAnalyticsBeacons();
-      cy.visit(url);
+  it('should send a view event for the Related Content component', async () => {
+    await scrollIntoView('[data-e2e="related-content-heading"]');
 
-      cy.get('[data-e2e="related-content-heading"]').scrollIntoView({
-        duration: 1000,
-      });
-
-      assertATIComponentViewEvent({
-        component: RELATED_CONTENT,
-        pageIdentifier,
-        contentType,
-        useReverb,
-      });
+    assertATIComponentViewEvent({
+      component: RELATED_CONTENT,
+      pageIdentifier,
+      contentType,
+      useReverb,
     });
   });
 };
@@ -32,27 +25,16 @@ export const assertRelatedContentComponentClick = ({
   contentType,
   useReverb,
 }) => {
-  it('should send a click event for the Related Content component', () => {
-    cy.url().then(url => {
-      interceptATIAnalyticsBeacons();
-      cy.visit(url);
+  it('should send a click event for the Related Content component', async () => {
+    await scrollIntoView('[data-e2e="related-content-heading"]');
 
-      cy.get('[data-e2e="related-content-heading"]').scrollIntoView({
-        duration: 1000,
-      });
+    await click('[data-e2e="related-content-heading"] a');
 
-      // Click on first item
-      cy.get('[data-e2e="related-content-heading"]').find('a').first().click();
-
-      assertATIComponentClickEvent({
-        component: RELATED_CONTENT,
-        pageIdentifier,
-        contentType,
-        useReverb,
-      });
-
-      // return to previous page
-      cy.visit(url);
+    assertATIComponentClickEvent({
+      component: RELATED_CONTENT,
+      pageIdentifier,
+      contentType,
+      useReverb,
     });
   });
 };
