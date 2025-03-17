8f2ee57a41c72d3b84669408bf4c23b08a71d8b1
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-17 12:20:47 +0000

    Migrate podcastLinks assertions to puppeteer

--- puppeteer/atiAnalytics/assertions/podcastLinks.js
+++ puppeteer/atiAnalytics/assertions/podcastLinks.js
@@ -1,4 +1,4 @@
-import { interceptATIAnalyticsBeacons, COMPONENTS } from '../helpers';
+import { click, COMPONENTS, scrollIntoView } from '../helpers';
 import { assertATIComponentClickEvent, assertATIComponentViewEvent } from '.';
 
 const { PODCAST_LINKS } = COMPONENTS;
@@ -8,21 +8,14 @@ export const assertPodcastLinksComponentView = ({
   contentType,
   useReverb,
 }) => {
-  it('should send a view event for the Podcast Links component', () => {
-    cy.url().then(url => {
-      interceptATIAnalyticsBeacons();
-      cy.visit(url);
+  it('should send a view event for the Podcast Links component', async () => {
+    await scrollIntoView('[data-e2e="podcast-links"]');
 
-      cy.get('[data-e2e="podcast-links"]').scrollIntoView({
-        duration: 1000,
-      });
-
-      assertATIComponentViewEvent({
-        component: PODCAST_LINKS,
-        pageIdentifier,
-        contentType,
-        useReverb,
-      });
+    assertATIComponentViewEvent({
+      component: PODCAST_LINKS,
+      pageIdentifier,
+      contentType,
+      useReverb,
     });
   });
 };
@@ -32,27 +25,17 @@ export const assertPodcastLinksComponentClick = ({
   contentType,
   useReverb,
 }) => {
-  it('should send a click event for the Podcast Links component', () => {
-    cy.url().then(url => {
-      interceptATIAnalyticsBeacons();
-      cy.visit(url);
+  it('should send a click event for the Podcast Links component', async () => {
+    await scrollIntoView('[data-e2e="podcast-links"]');
 
-      cy.get('[data-e2e="podcast-links"]').scrollIntoView({
-        duration: 1000,
-      });
+    // Click on the first link
+    await click('[data-e2e="podcast-links"] a');
 
-      // Click on the RSS link
-      cy.get('[data-e2e="podcast-links"]').contains('RSS').click();
-
-      assertATIComponentClickEvent({
-        component: PODCAST_LINKS,
-        pageIdentifier,
-        contentType,
-        useReverb,
-      });
-
-      // return to previous page
-      cy.visit(url);
+    assertATIComponentClickEvent({
+      component: PODCAST_LINKS,
+      pageIdentifier,
+      contentType,
+      useReverb,
     });
   });
 };
