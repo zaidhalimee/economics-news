ddd0038d43398608eb4be9f3af065aadc0ce438f
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-17 12:29:23 +0000

    Migrate recentAudioEpisodes assertions to puppeteer

--- puppeteer/atiAnalytics/assertions/recentAudioEpisodes.js
+++ puppeteer/atiAnalytics/assertions/recentAudioEpisodes.js
@@ -1,4 +1,4 @@
-import { interceptATIAnalyticsBeacons, COMPONENTS } from '../helpers';
+import { COMPONENTS, click, scrollIntoView } from '../helpers';
 import { assertATIComponentClickEvent, assertATIComponentViewEvent } from '.';
 
 const { RECENT_AUDIO_EPISODES } = COMPONENTS;
@@ -8,21 +8,14 @@ export const assertRecentAudioEpisodesComponentView = ({
   contentType,
   useReverb,
 }) => {
-  it('should send a view event for the Recent Audio Episodes component', () => {
-    cy.url().then(url => {
-      interceptATIAnalyticsBeacons();
-      cy.visit(url);
+  it('should send a view event for the Recent Audio Episodes component', async () => {
+    await scrollIntoView('[data-e2e="recent-episodes-list"]');
 
-      cy.get('[data-e2e="recent-episodes-list"]').scrollIntoView({
-        duration: 1000,
-      });
-
-      assertATIComponentViewEvent({
-        component: RECENT_AUDIO_EPISODES,
-        pageIdentifier,
-        contentType,
-        useReverb,
-      });
+    assertATIComponentViewEvent({
+      component: RECENT_AUDIO_EPISODES,
+      pageIdentifier,
+      contentType,
+      useReverb,
     });
   });
 };
@@ -32,53 +25,17 @@ export const assertRecentAudioEpisodesComponentClick = ({
   contentType,
   useReverb,
 }) => {
-  it('should send a click event for the Recent Audio Episodes component', () => {
-    cy.url().then(url => {
-      interceptATIAnalyticsBeacons();
-      cy.visit(url);
-      console.log(
-        'the url in assertRecentAudioEpisodesComponentClick before the click is',
-        url,
-      );
-      cy.log(
-        'the url in assertRecentAudioEpisodesComponentClick before the click is',
-        url,
-      );
+  it('should send a click event for the Recent Audio Episodes component', async () => {
+    await scrollIntoView('[data-e2e="recent-episodes-list"]');
 
-      cy.get('[data-e2e="recent-episodes-list"]').scrollIntoView({
-        duration: 1000,
-      });
+    // Click on first item
+    await click('[data-e2e="recent-episodes-list"] a');
 
-      // Click on first item
-      cy.get('[data-e2e="recent-episodes-list"]').find('a').first().click();
-
-      assertATIComponentClickEvent({
-        component: RECENT_AUDIO_EPISODES,
-        pageIdentifier,
-        contentType,
-        useReverb,
-      });
-      cy.url().then(url2 => {
-        console.log(
-          'the url in assertRecentAudioEpisodesComponentClick after the click is',
-          url2,
-        );
-        cy.log(
-          'the url in assertRecentAudioEpisodesComponentClick after the click is',
-          url2,
-        );
-      });
-      // return to previous page
-      cy.visit(url);
-
-      console.log(
-        'the url in assertRecentAudioEpisodesComponentClick after the revisiting of the previous page is',
-        url,
-      );
-      cy.log(
-        'the url in assertRecentAudioEpisodesComponentClick after the revisiting of the previous page is',
-        url,
-      );
+    assertATIComponentClickEvent({
+      component: RECENT_AUDIO_EPISODES,
+      pageIdentifier,
+      contentType,
+      useReverb,
     });
   });
 };
