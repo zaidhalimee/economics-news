4fa260fb83f22ec2aa5e405c2ccfe0db682018d9
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-18 18:12:21 +0000

    Refactor bundle requests tests to use the puppeteer test runner

--- puppeteer/bundleRequests/index.test.js
+++ puppeteer/bundleRequests/index.test.js
@@ -1,112 +1,50 @@
-// eslint-disable-next-line import/no-unresolved
-import puppeteer from 'puppeteer';
-import { localBaseUrl } from '#testHelpers/config';
+/* eslint-disable no-underscore-dangle */
 import shouldSmokeTest from '../../cypress/support/helpers/shouldSmokeTest';
 import serviceHasPageType from '../../cypress/support/helpers/serviceHasPageType';
+import config from '../../cypress/support/config/services';
 import getPaths from '../../cypress/support/helpers/getPaths';
+import {
+  assertLoadsExpectedBundles,
+  assertLoadsModernBundles,
+} from './assertions';
+import runTestsForPage from '../runTestsForPage';
+import context from '../context';
 
 global.Cypress = { env: () => 'local' };
-
-const config = require('../../cypress/support/config/services');
-
-let browser;
-let page;
-let requests = [];
-
-const isJsBundle = url => url.includes(localBaseUrl);
 
 const TIMEOUT = 60000;
 
 jest.setTimeout(TIMEOUT); // overriding the default jest timeout
 
-const getServiceBundleRegex = service => {
-  const SHARED_RUSSIAN_UKRAINIAN = 'shared-russian-ukrainian';
-
-  switch (service) {
-    case 'russian':
-      return SHARED_RUSSIAN_UKRAINIAN;
-    case 'ukrainian':
-      return `${service}|${SHARED_RUSSIAN_UKRAINIAN}`;
-    default:
-      return service;
+const onPageRequest = request => {
+  if (!context.page.__requests) {
+    context.page.__requests = [];
   }
+  context.page.__requests.push(request.url());
 };
 
-describe('Js bundle requests', () => {
-  beforeAll(async () => {
-    browser = await puppeteer.launch({
-      args: ['--no-sandbox'],
-    });
-    page = await browser.newPage();
-
-    page.setDefaultNavigationTimeout(TIMEOUT);
-
-    page.on('request', request => {
-      requests.push(request.url());
-    });
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  Object.keys(config).forEach(service => {
-    Object.keys(config[service].pageTypes)
+const testSuites = Object.keys(config)
+  .map(serviceId => {
+    return Object.keys(config[serviceId].pageTypes)
       .filter(
         pageType =>
-          shouldSmokeTest(pageType, service) &&
-          serviceHasPageType(service, pageType),
+          shouldSmokeTest(pageType, serviceId) &&
+          serviceHasPageType(serviceId, pageType),
       )
-      .forEach(pageType => {
-        const paths = getPaths(service, pageType);
+      .map(pageType => {
+        const paths = getPaths(serviceId, pageType);
 
-        if (paths.length > 0) {
-          const path = paths[0];
+        const [path] = paths;
 
-          describe(service, () => {
-            beforeAll(async () => {
-              await page.goto(`${localBaseUrl}${path}`, {
-                waitUntil: 'networkidle2',
-              });
-            });
-
-            afterAll(() => {
-              requests = [];
-            });
-
-            it('only loads expected js bundles', () => {
-              const serviceRegex = getServiceBundleRegex(config[service].name);
-
-              requests
-                .filter(url => url.endsWith('.js'))
-                .filter(isJsBundle)
-                .forEach(url => {
-                  expect(url).toMatch(
-                    new RegExp(
-                      `(\\/static\\/js\\/(?:comscore\\/)?(modern.)?(main|framework|commons|shared|${serviceRegex}|frosted_promo|themes|.+Page).+?.js)|(\\/static\\/.+?-lib.+?.js)|${config[service].name}\\/(articles\\/)?sw\\.js`,
-                      'g',
-                    ),
-                  );
-                });
-            });
-
-            it('loads at least 1 modern service bundle', () => {
-              const serviceRegex = getServiceBundleRegex(config[service].name);
-              const serviceMatches = requests.filter(url =>
-                url.match(
-                  new RegExp(
-                    `(\\/static\\/js\\/modern.${serviceRegex}.+?.js)`,
-                    'g',
-                  ),
-                ),
-              );
-
-              expect(serviceMatches.length).toBeGreaterThanOrEqual(1);
-            });
-          });
-        } else {
-          describe(`No bundle request tests for ${service} ${pageType}`, () => {});
-        }
+        return {
+          path,
+          pageType,
+          service: config[serviceId].name,
+          runforEnv: ['local'],
+          tests: [assertLoadsExpectedBundles, assertLoadsModernBundles],
+        };
       });
-  });
-});
+  })
+  .flat();
+
+runTestsForPage({ testSuites, onPageRequest });
