dae92d2018b4a0682ccf125c1118652904d8145d
Karina Thomas <karina.thomas@bbc.co.uk>
2025-03-20 10:01:57 +0000

    Add logging to service worker

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,21 @@
+/* eslint-disable no-console */
 /* eslint-disable no-useless-return */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
-const version = 'v0.2.1a';
+const version = 'v0.2.2';
 const cacheName = 'simorghCache_v1';
 
 const service = self.location.pathname.split('/')[1];
 
 self.addEventListener('install', event => {
+  console.log(`Service worker ${version} installed`);
   event.waitUntil(caches.open(cacheName));
+});
+
+self.addEventListener('activated', event => {
+  console.log(`Service worker ${version} activated`);
 });
 
 const fetchEventHandler = async event => {
@@ -43,6 +49,10 @@ const fetchEventHandler = async event => {
   ) {
     const cache = await caches.open(cacheName);
     let response = await cache.match(event.request);
+
+    console.log(
+      `Response for ${event.request.url} was ${response ? '' : ' not '} in the cache`,
+    );
 
     if (!response) {
       response = await fetch(event.request.url);
