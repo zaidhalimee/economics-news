56d94af8a6ad859808a6711976786ed5691eec80
“LilyL0u” <lily.higham@bbc.co.uk>
2025-03-24 18:31:39 +0000

    read more not full page width

--- src/app/pages/ArticlePage/ReadMoreButton/index.styles.ts
+++ src/app/pages/ArticlePage/ReadMoreButton/index.styles.ts
@@ -2,13 +2,14 @@ import NO_JS_CLASSNAME from '#app/lib/noJs.const';
 import { css, Theme } from '@emotion/react';
 
 export default {
-  readMoreButton: ({ spacings }: Theme) =>
+  readMoreButton: ({ spacings, mq }: Theme) =>
     css({
       cursor: 'pointer',
       fontWeight: 'bold',
       display: 'block',
-      width: '100%',
+      width: 'calc(100% - 2rem)',
       padding: `${spacings.DOUBLE}rem 0`,
+      margin: `auto ${spacings.DOUBLE}rem`,
       backgroundColor: '#141414',
       color: '#FFFFFF',
       border: 'none',
