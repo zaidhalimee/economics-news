06c61f61585c92252586ad69a244d423927955a1
emilysaffron <emily.saffron@bbc.co.uk>
2025-03-25 13:55:57 +0000

    rename pagetypes

--- scripts/litePageSizeValidator/index.js
+++ scripts/litePageSizeValidator/index.js
@@ -5,65 +5,64 @@ const MAX_PAGE_SIZE_KB = 100;
 
 const litePageSizeValidator = async () => {
   const urlsToCheck = [
-    { path: '/hindi', pageType: 'home' },
+    { path: '/hindi', pageType: 'Home' },
     { path: '/mundo/articles/cddylv9g8z0o', pageType: 'article' },
     {
       path: '/nepali/bbc_nepali_radio/liveradio',
-      pageType: 'live radio',
+      pageType: 'Live Radio',
     },
-    { path: '/arabic/media-53135426', pageType: 'media asset with live stream' },
-    { path: '/marathi/popular/read', pageType: 'most read' },
+    {
+      path: '/arabic/media-53135426',
+      pageType: 'Media Asset with Live Stream',
+    },
+    { path: '/marathi/popular/read', pageType: 'Most Read' },
     {
       path: '/gahuza/bbc_gahuza_radio/programmes/p0340x2m',
-      pageType: 'on demand audio - Brand',
+      pageType: 'On Demand Audio - Brand',
     },
     {
       path: '/gahuza/bbc_gahuza_radio/w3ct1v5v',
-      pageType: 'on demand audio - Episode',
+      pageType: 'On Demand Audio - Episode',
     },
     {
       path: '/gahuza/podcasts/p07yh8hb',
-      pageType: 'podcast - Brand',
+      pageType: 'Podcast - Brand',
     },
     {
       path: '/gahuza/podcasts/p07yh8hb/p094vs2n',
-      pageType: 'podcast - Episode',
+      pageType: 'Podcast - Episode',
     },
     {
       path: '/gujarati/bbc_gujarati_tv/tv_programmes/w13xttqr',
-      pageType: 'on demand tv - Brand',
+      pageType: 'On Demand TV - Brand',
     },
     {
       path: '/hausa/bbc_hausa_tv/tv/w172yjj7rfhxp1p',
-      pageType: 'on demand tv - Episode',
-    },
-    {
-      path: '/hausa/bbc_hausa_tv/tv/w172yjj7rfhxp1p',
-      pageType: 'on demand tv - Episode',
+      pageType: 'On Demand TV - Episode',
     },
     {
       path: '/tigrinya/news-51249937',
-      pageType: 'media article',
+      pageType: 'Media Article',
     },
     { path: '/nepali/news-50627370', pageType: 'CPS Photo Gallery (PGL)' },
     { path: '/arabic/sports-54278377', pageType: 'CPS Story (STY)' },
-    { path: '/korean/topics/cnwng7v0e54t', pageType: 'topic' },
+    { path: '/korean/topics/cnwng7v0e54t', pageType: 'Topic' },
 
     {
       path: '/urdu/live/c04z6x46l0vt',
-      pageType: 'live',
+      pageType: 'Live',
       nextjs: true,
     },
 
     {
       path: '/mundo/send/u50853489',
-      pageType: 'uploader',
+      pageType: 'Uploader',
       nextjs: true,
     },
 
     {
       path: '/ws/languages',
-      pageType: 'languages',
+      pageType: 'Languages',
       nextjs: true,
     },
   ];
