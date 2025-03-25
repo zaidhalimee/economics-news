f2a1df5fe95ba4ca0e5e133fb965291d1e4a5411
“LilyL0u” <lily.higham@bbc.co.uk>
2025-03-25 13:55:41 +0000

    font pica

--- src/app/pages/ArticlePage/ReadMoreButton/index.styles.ts
+++ src/app/pages/ArticlePage/ReadMoreButton/index.styles.ts
@@ -2,8 +2,10 @@ import NO_JS_CLASSNAME from '#app/lib/noJs.const';
 import { css, Theme } from '@emotion/react';
 
 export default {
-  readMoreButton: ({ spacings, mq }: Theme) =>
+  readMoreButton: ({ spacings, fontSizes, fontVariants }: Theme) =>
     css({
+      ...fontSizes.pica,
+      ...fontVariants.serifMedium,
       cursor: 'pointer',
       fontWeight: 'bold',
       display: 'block',
