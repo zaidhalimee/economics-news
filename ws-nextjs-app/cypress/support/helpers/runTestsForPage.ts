117f65b77bc8f6d28d5a83e0ece7616dc26022e1
emilysaffron <emily.saffron@bbc.co.uk>
2025-03-31 14:58:18 +0100

    first draft at intercepting pagesize with cypress

--- ws-nextjs-app/cypress/support/helpers/runTestsForPage.ts
+++ ws-nextjs-app/cypress/support/helpers/runTestsForPage.ts
@@ -2,13 +2,41 @@ export default ({ pageType, testSuites, testIsolation = false }) => {
   testSuites.forEach(testData => {
     const { path, tests, runforEnv, ...params } = testData;
     const cypressEnv = Cypress.env('APP_ENV');
+
     if (runforEnv.includes(cypressEnv)) {
       describe(`${Cypress.config().baseUrl}${path}`, { testIsolation }, () => {
+        let pageSize;
+        let chartbeatSize;
+
         before(() => {
+          cy.intercept('GET', path, req => {
+            req.headers['Cache-Control'] = 'no-cache';
+          }).as('pageRequest');
+
+          cy.intercept(
+            'GET',
+            'http://static.chartbeat.com/js/chartbeat.js',
+            req => {
+              req.headers['Cache-Control'] = 'no-cache';
+            },
+          ).as('chartbeatRequest');
+
           cy.visit(path);
+
+          cy.wait(['@pageRequest', '@chartbeatRequest']).then(interceptions => {
+            pageSize = interceptions[0].response.body.length / 1024;
+            chartbeatSize = interceptions[1].response.body.length / 1024;
+
+            console.log('Page size:', pageSize);
+            console.log('Chartbeat size:', chartbeatSize);
+            Cypress.env('PAGE_SIZE', pageSize);
+            Cypress.env('CHARTBEAT_SIZE', chartbeatSize);
+          });
         });
 
-        tests.forEach(test => test({ path, pageType, ...params }));
+        tests.forEach(test => {
+          test({ path, pageType, ...params });
+        });
       });
     }
   });
