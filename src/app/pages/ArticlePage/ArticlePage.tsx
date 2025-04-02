9298d752f399f016d5784793c1719070918fa97f
Aaron Moore <aaron.moore@bbc.co.uk>
2025-04-02 19:40:14 +0100

    Update ArticlePage.tsx

--- src/app/pages/ArticlePage/ArticlePage.tsx
+++ src/app/pages/ArticlePage/ArticlePage.tsx
@@ -40,7 +40,6 @@ import {
   Recommendation,
 } from '#app/models/types/optimo';
 import ScrollablePromo from '#components/ScrollablePromo';
-import Recommendations from '#app/components/Recommendations';
 import ElectionBanner from './ElectionBanner';
 import ImageWithCaption from '../../components/ImageWithCaption';
 import AdContainer from '../../components/Ad';
@@ -101,12 +100,10 @@ const getMpuComponent =
     allowAdvertising ? <AdContainer {...props} slotType="mpu" /> : null;
 
 const getWsojComponent =
-  (recommendationsData: Recommendation[]) => (props: ComponentToRenderProps) =>
-    props?.data ? (
-      <Recommendations data={props.data} />
-    ) : (
-      <CpsRecommendations {...props} items={recommendationsData} />
-    );
+  (recommendationsData: Recommendation[]) =>
+  (props: ComponentToRenderProps) => (
+    <CpsRecommendations {...props} items={recommendationsData} />
+  );
 
 const DisclaimerWithPaddingOverride = (props: ComponentToRenderProps) => (
   <Disclaimer {...props} increasePaddingOnDesktop={false} />
