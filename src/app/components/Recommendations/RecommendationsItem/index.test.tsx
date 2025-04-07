87a0ee6bde90e9a1426e95704ec7bfbc0f9f4fa7
Aaron Moore <aaron.moore@bbc.co.uk>
2025-04-07 11:59:58 +0100

    Update fixture in test + update ati E2E component name

--- src/app/components/Recommendations/RecommendationsItem/index.test.tsx
+++ src/app/components/Recommendations/RecommendationsItem/index.test.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import { render } from '#app/components/react-testing-library-with-providers';
 import RecommendationsItem from '.';
-
-const recommendationFixture = {
-  id: '123',
-  title: 'शेख़ मुजीब और इंदिरा गांधी के बीच हुए समझौते पर क्यों हुआ था विवाद?',
-  image: {
-    width: 1824,
-    height: 1026,
-    altText: 'सुनीता विलियम्स',
-    locator: 'a095/live/9c11d5e0-0581-11f0-88b7-5556e7b55c5e.jpg',
-    originCode: 'cpsprodpb',
-    copyrightHolder: 'Getty Images',
-  },
-  href: 'https://www.bbc.co.uk',
-};
+import recommendationsFixtures from '../fixtures';
 
 describe('RecommendationsItem', () => {
   it('should render a single recommendation with a title and link', () => {
     const { getByText, container } = render(
-      <RecommendationsItem recommendation={recommendationFixture} />,
+      <RecommendationsItem recommendation={recommendationsFixtures[0]} />,
     );
 
     const links = container.querySelectorAll('a');
 
-    expect(getByText(recommendationFixture.title)).toBeInTheDocument();
+    expect(getByText(recommendationsFixtures[0].title)).toBeInTheDocument();
     expect(links).toHaveLength(1);
-    expect(links[0].getAttribute('href')).toEqual(recommendationFixture.href);
+    expect(links[0].getAttribute('href')).toEqual(
+      recommendationsFixtures[0].href,
+    );
   });
 
   it('should not render if no recommendation is provided', () => {
