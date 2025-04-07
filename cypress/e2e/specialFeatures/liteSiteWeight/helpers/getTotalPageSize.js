0db785326085d0265ecd920644cff897c8612158
emilysaffron <emily.saffron@bbc.co.uk>
2025-04-07 10:39:45 +0100

    conditionally gzip

--- cypress/e2e/specialFeatures/liteSiteWeight/helpers/getTotalPageSize.js
+++ cypress/e2e/specialFeatures/liteSiteWeight/helpers/getTotalPageSize.js
@@ -8,12 +8,12 @@ export default requests => {
   // eslint-disable-next-line cypress/unsafe-to-chain-command
   return cy
     .wrap(requests)
-    .each(({ url, contentLength }) => {
+    .each(({ url, isCompressed, contentLength }) => {
       if (contentLength) {
         requestSizes.push({ url, size: contentLength });
         totalSize += contentLength;
       } else {
-        getPageSizeInKB(url).then(size => {
+        getPageSizeInKB(url, isCompressed).then(size => {
           requestSizes.push({ url, size });
           totalSize += size;
         });
